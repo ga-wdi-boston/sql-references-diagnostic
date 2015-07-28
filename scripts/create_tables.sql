adf79f42c1693b4cfbf4ad23098fcc18e0fd660e
jlevinger <joe@joelevinger.com>
2015-07-28 16:31:05 -0400

    answers to quiz

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,21 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(id SERIAL PRIMARY KEY,
+                     surname TEXT,
+                     given_name TEXT,
+                     gender TEXT,
+                     dob TEXT,
+                     specialty TEXT);
+
+CREATE TABLE patients(id SERIAL PRIMARY KEY,
+                     surname TEXT,
+                     given_name TEXT,
+                     gender TEXT,
+                     dob TEXT,
+                     insurance TEXT);
+
+CREATE TABLE appointments(id SERIAL PRIMARY KEY,
+                     appt_date TEXT,
+                     time TEXT,
+                     comment TEXT,
+                     doctor_id INTEGER,
+                     patient_id INTEGER);
