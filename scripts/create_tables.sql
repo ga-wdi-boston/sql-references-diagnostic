7b23714afea1fa161838f6c7f0f9f85b1de29f71
Antony Donovan <antony.donovan@generalassemb.ly>
2015-07-28 09:52:40 -0400

    Add solution to assessment
    Create examples for grader and students.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,4 @@
 -- create doctors, patients, and appointments tables
+create table doctors(id serial primary key, surname text,given_name text,gender text,dob text,specialty text);
+create table patients(id serial primary key, surname text,given_name text,gender text,dob text,insurance text);
+create table appointments(date text, time text, comment text, doctor_id integer references doctors(id), patient_id integer references patients(id));
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,16 @@
 -- retrieve appointment information
+select p.surname, a.date, a.time
+from patients p
+inner join appointments a
+  on a.patient_id = p.id
+inner join doctors d
+  on a.doctor_id = d.id
+where d.surname = 'Hubbard';
+
+select d.surname, a.date, a.time
+from doctors d
+inner join appointments a
+  on a.doctor_id = d.id
+inner join patients p
+  on a.patient_id = p.id
+where p.surname = 'Wilcox';
