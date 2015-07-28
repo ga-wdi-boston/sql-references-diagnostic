27917ca212de227ddfc0822d66902d4fb49ded8e
arvindeva <arvindeva@gmail.com>
2015-07-28 16:30:01 -0400

    fixed select_appointments

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,13 +1,5 @@
 -- retrieve appointment information
 -- Question 4. Retrieve the names and appointment times for everyone seeing Dr. Hubbard.  Retrieve the Doctors' names and appointment times for Marsha Wilcox.
--- SELECT d.surname, d.given_name, c.name, c.country, j.start_date, j.end_date
--- FROM developers d
--- INNER JOIN jobs j
---   ON d.id = j.developer_id
--- INNER JOIN companies c
---   ON j.company_id = c.id
--- WHERE c.name = 'HP'
--- AND j.start_date = '20150913';
 
 SELECT p.surname, p.given_name, a.time, a.date
 FROM patients p
