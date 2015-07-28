51b743258bd14249bd32b1bcd7d334c3f51a236a
Bernard Lee <bernardlee82@gmail.com>
2015-07-28 16:54:30 -0400

    Removed extraneous comments.

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -12,25 +12,3 @@ INNER JOIN appointments a
 	ON d.id = a.doctor_id
 WHERE a.patient_id = 8
 ;
--- Retrieve the names and appointment times for everyone seeing Dr. Hubbard.
--- Retrieve the Doctors' names and appointment times for Marsha Wilcox.
-
--- SELECT p.surname, p.given_name
--- FROM people p
--- INNER JOIN endorsements e
--- 	ON p.id = e.person_id
--- INNER JOIN skills s
--- 	ON e.skill_id = s.id
--- WHERE language = 'Ruby'
--- AND e.years_experience > 2
--- ;
---
--- SELECT s.language, s.framework, e.years_experience
--- FROM skills s
--- INNER JOIN endorsements e
--- 	ON s.id = e.skill_id
--- INNER JOIN people p
--- 	ON e.person_id = p.id
--- WHERE p.surname = 'Walden'
--- AND p.given_name = 'Kirby'
--- ;
