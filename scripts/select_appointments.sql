4f544707fcb6afcbb2af88fc5121c0b0defd1b27
aemassa <annaemassa@gmail.com>
2015-07-28 16:31:18 -0400

    Submits assessment with SQL scripts.
    Not finished!

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,11 @@
 -- retrieve appointment information
+
+
+SELECT p.surname, p.given_name, a.time
+FROM patients p
+INNER JOIN appointments a
+  ON p.id = a.patient_id
+INNER JOIN doctors d
+  ON d.id =
+
+-- not done!
