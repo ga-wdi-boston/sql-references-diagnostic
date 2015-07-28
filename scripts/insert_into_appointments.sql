750b91b0bcd068880aa49f7a75730c819f1bf83a
caraclarke <cara.clarke8@gmail.com>
2015-07-28 16:30:42 -0400

    Assessment completed

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,19 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments(date, time, patient_id, doctor_id)
+  SELECT '20150812','10:00',
+    (SELECT id FROM patients WHERE surname = 'Wilcox' AND given_name = 'Marsha'),
+    (SELECT id FROM doctors WHERE surname = 'Hilton' AND given_name = 'Vicky')
+  UNION
+  SELECT '20150817','13:00',
+    (SELECT id FROM patients WHERE surname = 'Talley' AND given_name = 'Ivory'),
+    (SELECT id FROM doctors WHERE surname = 'Hubbard' AND given_name = 'Spencer')
+  UNION
+  SELECT '20150812','10:00',
+    (SELECT id FROM patients WHERE surname = 'Wilcox' AND given_name = 'Marsha'),
+    (SELECT id FROM doctors WHERE surname = 'Hubbard' AND given_name = 'Spencer')
+  UNION
+  SELECT '20150817', '13:00',
+    (SELECT id FROM patients WHERE surname = 'Hodge' AND given_name = 'Blake'),
+    (SELECT id FROM doctors WHERE surname = 'Hilton' AND given_name = 'Vicky')
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,20 @@
 -- retrieve appointment information
+
+SELECT p.surname, p.given_name, d.surname, d.given_name, a.time
+  FROM patients p
+  INNER JOIN appointments a
+    ON p.id = a.patient_id
+  INNER JOIN doctors d
+    ON d.id = a.doctor_id
+  WHERE d.surname = 'Hubbard'
+;
+
+SELECT p.surname, p.given_name, d.surname, d.given_name, a.time
+  FROM doctors d
+  INNER JOIN appointments a
+    ON d.id = a.doctor_id
+  INNER JOIN patients p
+    ON p.id = a.patient_id
+  WHERE p.surname = 'Wilcox'
+    AND p.given_name = 'Marsha'
+;
