5e68014f042954ea46ab3e22c94da3e06c9cfe87
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-07-28 16:30:13 -0400

    finishes assessment, tested 3 files out of 4.

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,20 @@
 -- retrieve appointment information
+SELECT d.surname, d.given_name, p.surname, p.given_name, a.date, a.time
+FROM patients p
+INNER JOIN appointments a
+  ON p.id = a.patients_id
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE d.surname = 'Hubbard'
+  AND d.given_name = 'Spencer'
+;
+
+SELECT d.surname, d.given_name, p.surname, p.given_name, a.date, a.time
+FROM doctors d
+INNER JOIN appointments a
+  ON d.id = a.doctor_id
+INNER JOIN patients p
+  ON a.patient_id = p.id
+WHERE p.surname = 'Wilcox'
+  AND p.given_name = 'Marsha'
+;
