c555cde6643fe2b0c7bfc2c7c57ef6d93dc4ac5b
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-28 16:27:53 -0400

    Retrieves required information
    Retrieves names and appointment times for those seeing Dr. Hubbard
    Retrieves doctors' names and appointment times for Marsha Wilcox

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,18 @@
 -- retrieve appointment information
+SELECT p.surname, p.given_name, a.date, a.time
+FROM patients p
+INNER JOIN appointments a
+  ON p.id = a.patient_id
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE d.surname = 'Hubbard'
+;
+
+SELECT d.surname, d.given_name, a.date, a.time
+FROM doctors d
+INNER JOIN appointments a
+  ON d.id = a.doctor_id
+INNER JOIN patients p
+  ON a.patient_id = p.id
+WHERE p.surname = 'Wilcox' AND p.given_name = 'Marsha'
+;
