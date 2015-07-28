09367f8fa83639e1fddce7e0587fd49bdcc9de00
lekova <lina.lekova@gmail.com>
2015-07-28 16:29:09 -0400

    Compeleted assessment

--- scripts/load_doctors_and_patients.sql
+++ scripts/load_doctors_and_patients.sql
@@ -1,1 +1,5 @@
 -- load doctors and patients tables from csv files
+\copy doctors(surname,given_name,gender,dob,specialty) FROM 'data/doctors.csv' WITH (FORMAT csv, HEADER TRUE);
+
+\copy patients(surname,given_name,gender,dob,insurance) FROM 'data/patients.csv' WITH (FORMAT csv, HEADER TRUE);
+
