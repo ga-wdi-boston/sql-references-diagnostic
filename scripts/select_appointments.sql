7064163168fcbc9dacdd14b2e94238fb7963e469
kristenlk <klkehlenbeck@gmail.com>
2015-07-28 16:24:04 -0400

    Completes assessment.

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,17 @@
 -- retrieve appointment information
+
+SELECT p.surname, p.given_name, a.date, a.time
+FROM doctors d
+INNER JOIN appointments a
+  ON d.id = a.doctor_id
+INNER JOIN patients p
+  ON p.id = a.patient_id
+WHERE d.surname = 'Hubbard';
+
+SELECT d.surname, d.given_name, a.date, a.time
+FROM patients p
+INNER JOIN appointments a
+  ON p.id = a.patient_id
+INNER JOIN doctors d
+  ON d.id = a.doctor_id
+WHERE p.surname = 'Wilcox' AND p.given_name = 'Marsha';
