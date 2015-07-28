a61d3077e66faaae0aafb48b72f5a4dba687deb3
arvindeva <arvindeva@gmail.com>
2015-07-28 16:28:50 -0400

    Finished Solo Assessment

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,23 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, doctor_id, patient_id)
+  SELECT '20150812', '10:00',
+    (SELECT id FROM doctors WHERE given_name = 'Vickie' AND surname = 'Hilton'),
+    (SELECT id FROM patients WHERE given_name = 'Marsha' AND surname = 'Wilcox')
+  UNION
+  SELECT '20150817', '13:00',
+    (SELECT id FROM doctors WHERE given_name = 'Spencer' AND surname = 'Hubbard'),
+    (SELECT id FROM patients WHERE given_name = 'Ivory' AND surname = 'Talley')
+  UNION
+  SELECT '20150812', '10:00',
+    (SELECT id FROM doctors WHERE given_name = 'Spencer' AND surname = 'Hubbard'),
+    (SELECT id FROM patients WHERE given_name = 'Marsha' AND surname = 'Wilcox')
+  UNION
+  SELECT '20150817', '13:00',
+    (SELECT id FROM doctors WHERE given_name = 'Vickie' AND surname = 'Hilton'),
+    (SELECT id FROM patients WHERE given_name = 'Blake' AND surname = 'Hodge')
+    ;
+
+-- Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2015.
+-- Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2015.
+-- Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2015.
+-- Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2015.
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,26 @@
 -- retrieve appointment information
+-- Question 4. Retrieve the names and appointment times for everyone seeing Dr. Hubbard.  Retrieve the Doctors' names and appointment times for Marsha Wilcox.
+-- SELECT d.surname, d.given_name, c.name, c.country, j.start_date, j.end_date
+-- FROM developers d
+-- INNER JOIN jobs j
+--   ON d.id = j.developer_id
+-- INNER JOIN companies c
+--   ON j.company_id = c.id
+-- WHERE c.name = 'HP'
+-- AND j.start_date = '20150913';
+
+SELECT p.surname, p.given_name, a.time, a.date
+FROM patients p
+INNER JOIN appointments a
+  ON p.id = a.patient_id
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE d.surname = 'Hubbard';
+
+SELECT d.surname, d.given_name, a.time, a.date
+FROM patients p
+INNER JOIN appointments a
+  ON p.id = a.patient_id
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE p.surname = 'Wilcox';
