c9494b0a09233aeb37a6f6b8cc4efd9c083fc523
Antony Donovan <antony.donovan@generalassemb.ly>
2015-11-02 22:13:08 -0500

    Uppercase SQL keywords
    Add consistency of usage

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,4 +1,25 @@
 -- create doctors, patients, and appointments tables
-create table doctors(id serial primary key, surname text,given_name text,gender text,dob text,specialty text);
-create table patients(id serial primary key, surname text,given_name text,gender text,dob text,insurance text);
-create table appointments(date text, time text, comment text, doctor_id integer references doctors(id), patient_id integer references patients(id));
+-- create doctors
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  dob TEXT,
+  specialty TEXT
+);
+-- create patients
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  dob TEXT,
+  insurance TEXT
+);
+-- create appointments
+CREATE TABLE appointments(
+  date TEXT, time TEXT, comment TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,16 +1,17 @@
 -- retrieve appointment information
-select p.surname, a.date, a.time
-from patients p
-inner join appointments a
-  on a.patient_id = p.id
-inner join doctors d
-  on a.doctor_id = d.id
-where d.surname = 'Hubbard';
-
-select d.surname, a.date, a.time
-from doctors d
-inner join appointments a
-  on a.doctor_id = d.id
-inner join patients p
-  on a.patient_id = p.id
-where p.surname = 'Wilcox';
+-- for Dr. Hubbard
+SELECT p.surname, a.date, a.time
+FROM patients p
+INNER JOIN appointments a
+  ON a.patient_id = p.id
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE d.surname = 'Hubbard';
+-- for patient Wilcox
+SELECT d.surname, a.date, a.time
+FROM doctors d
+INNER JOIN appointments a
+  ON a.doctor_id = d.id
+INNER JOIN patients p
+  ON a.patient_id = p.id
+WHERE p.surname = 'Wilcox';
