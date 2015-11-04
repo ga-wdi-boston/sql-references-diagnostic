7f4d15d52e1abe5e08243384bc2c2ef2970ec5b6
Holden Mills <mholdenmills@gmail.com>
2015-11-04 09:38:55 -0500

    Answered q's

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,6 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(patient_id, doctor_id, time, date, comment)
+  VALUES ( [Patient id of 'Marsha Wilcox'], [doctor id of 'Dr. Vicky Hilton'], 10:00, 08-12-2016)
+         ([Patient id of 'Ivory Talley'], [doctor id of 'Dr. Spencer Hubbard'], 13:00 08-17-2016)
+  + Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+  + Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
