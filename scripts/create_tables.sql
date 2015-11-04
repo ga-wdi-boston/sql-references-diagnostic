d7c91930b6b0c5d8b447a1d3a68a26f1c8404e8a
Jeff Meyer <jcmeyer10@gmail.com>
2015-11-04 09:37:52 -0500

    "All four edited files for assessment included"

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY
+  surname CHARACTER VARYING,
+  given_name CHARACTER VARYING,
+  gender CHARACTER VARYING,
+  dob DATE,
+  specialty CHARACTER VARYING
+);
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY
+  surname CHARACTER VARYING,
+  given_name CHARACTER VARYING,
+  gender CHARACTER VARYING,
+  dob DATE,
+  insurance CHARACTER VARYING
+);
+
+CREATE TABLE appointment (
+  id SERIAL PRIMARY KEY
+  appt_date TEXT,
+  time TEXT,
+  comment TEXT,
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctor(id)
+);
