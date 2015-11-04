8dfb67760cf26df5c7ade979174082c9c196acb3
wiltrahan <twilorip@gmail.com>
2015-11-04 09:34:46 -0500

    completed sql-join-tables-assessment

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,26 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+id SERIAL PRIMARY KEY,
+surname CHARACTER VARYING,
+given_name CHARACTER VARYING,
+gender CHARACTER VARYYING(1),
+dob DATE,
+specialty CHARACTER VARYING
+);
+
+CREATE TABLE patients(
+id SERIAL PRIMARY KEY,
+surname CHARACTER VARYING,
+given_name CHARACTER VARYING,
+gender CHARACTER VARYING(1),
+dob DATE,
+insurance CHARACTER VARYING
+);
+
+CREATE TABLE appointments
+id SERIAL PRIMARY KEY,
+date DATE,
+time INTEGER,
+comment CHARACTER VARYING
+);
+
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(given_name, surname, doctor_id, datetime)
+  VALUES ('Marsha', 'Wilcox', 4, '2016-08-12 10:00:00'),
+         ('Ivory', 'Talley', 6 , '2016-08-17 13:00:00'),
+         ('Marsha', 'Wilcox', 6,'2016-08-12 10:00:00'),
+         ('Blake', 'Hodge', 4, '2016-08-17 13:00:00')
+;
