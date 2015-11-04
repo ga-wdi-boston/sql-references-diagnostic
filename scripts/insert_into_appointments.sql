6307a5411f2323c18931c869df007cf6f201355a
EdieShimel <edieshimel@gmail.com>
2015-11-04 09:37:37 -0500

    here we go answers

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,48 @@
 -- insert appointments creating connections between doctors and patients
+
+
+
+
+  SELECT 10am, 'August 12, 2016', 'New Appt',
+    (SELECT id
+      FROM patients
+      WHERE surname = 'Wilcox'
+      AND given_name = 'Marsha'),
+    (SELECT id
+      FROM doctors
+      WHERE surname = 'Hilton'
+      AND given_name = 'Vicky')
+  UNION
+    SELECT 1pm, 'August 17, 2016', 'New Appt',
+    (SELECT id
+      FROM patients
+      WHERE surname = 'Wilcox'
+      AND given_name = 'Marsha'),
+    (SELECT id
+      FROM doctors
+      WHERE surname = 'Hubbard'
+      AND given_name = 'Spencer')
+    UNION
+
+
+
+    SELECT 10am, 'August 12, 2016', 'New Appt',
+    (SELECT id
+      FROM patients
+      WHERE surname = 'Wilcox'
+      AND given_name = 'Marsha'),
+    (SELECT id
+      FROM doctors
+      WHERE surname = 'Hubbard'
+      AND given_name = 'Spencer')
+    UNION
+
+     SELECT 1pm, 'August 17, 2016', 'New Appt',
+    (SELECT id
+      FROM patients
+      WHERE surname = 'Hodge'
+      AND given_name = 'Blake'),
+    (SELECT id
+      FROM doctors
+      WHERE surname = 'Hilton'
+      AND given_name = 'Vicky')
