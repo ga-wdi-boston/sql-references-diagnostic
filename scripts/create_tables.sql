edc8606b54c7a8b8e79e87d43c1c2d62ddd9ace8
MikeKutter <mkutter@yahoo.com>
2015-11-04 09:35:32 -0500

    Completed Assessment

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,30 @@
 -- create doctors, patients, and appointments tables
+-- surname,given_name,gender,dob,specialty
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  surname CHARACTER VARYING,
+  given_name CHARACTER VARYING,
+  gender CHARACTER VARYING,
+  dob DATE,
+  specialty CHARACTER VARYING
+  );
+
+
+-- surname,given_name,gender,dob,insurance
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  surname CHARACTER VARYING,
+  given_name CHARACTER VARYING,
+  gender CHARACTER VARYING,
+  dob DATE,
+  insurance CHARACTER VARYING
+  );
+
+CREATE TABLE appointments (
+  id SERIAL PRIMARY KEY,
+  appointment_date TEXT,
+  appointment_time TEXT,
+  comments TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+  );
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,22 @@
 -- retrieve appointment information
+-- Question 4. Retrieve the names and appointment times for everyone seeing Dr. Hubbard. Retrieve the Doctors' names and appointment times for Marsha Wilcox.
+
+
+
+SELECT p.surname, p.given_name, p.appointment_time, p.appointment_date
+FROM patients p
+INNER JOIN appointments a
+  ON e.skill_id = s.id
+INNER JOIN doctors d
+  ON e.person_id = p.id
+WHERE p.surname = 'Worley'
+;
+
+SELECT s.framework, s.language
+FROM skills s
+INNER JOIN endorsements e
+  ON e.skill_id = s.id
+INNER JOIN people p
+  ON e.person_id = p.id
+WHERE p.surname = 'Worley'
+;
