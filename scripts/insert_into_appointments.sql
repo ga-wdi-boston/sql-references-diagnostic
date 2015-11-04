5ac687e68ecab3c247864e373fb4368db8c82ad4
resource11 <mcmahon@resource11.com>
2015-11-04 09:37:20 -0500

    Added responses for assessment

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,9 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments(patient_id, doctor_id, appt_time,
+  appt_date)
+  VALUES (8, 4, '10am', 'August 12, 2016'),
+         (5, 6, '1pm', 'August 17, 2016'),
+         (8, 6, '10am', 'August 12, 2016'),
+         (7, 4, '1pm', 'August 17, 2016')
+;
