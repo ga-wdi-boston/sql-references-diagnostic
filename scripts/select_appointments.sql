2c384905ec2c9b88155520fe3635f4d43db7421c
LaurisaNeuwirth <uberlaurisa@gmail.com>
2015-11-04 09:38:18 -0500

    Assessment, did not finish #4 SORRY!

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,15 @@
 -- retrieve appointment information
+-- Retrieve the names and appointment times for everyone seeing Dr. Hubbard.
+SELECT p.given_name, p.surname
+FROM patients p
+INNER JOIN appointments a
+  ON a.date = a.date
+
+-- NOT FINISHED!! SORRY!!!!
+-- INNER JOIN people p
+--   ON e.person_id = p.id
+-- WHERE p.surname = 'Adams'
+;
+
+-- Retrieve the Doctors' names and appointment times for Marsha Wilcox.
+
