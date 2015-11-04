702c209a012559e851ff753b60d6e0a66e5500de
Kaylal12 <kaylalei224@gmail.com>
2015-11-04 09:23:40 -0500

    Finished assessment

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,6 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(time, date_of_appointment, comment, doctor_id, patient_id)
+VALUES ('10am', 'August 12, 2016', '', 4, 8),
+       ('1pm', 'August 17, 2016', '', 6, 5),
+       ('10am', 'August 12, 2016', '', 6, 8),
+       ('1pm', 'August 17, 2016', '', 4, 7)
