85ca15512689b83b6c9861becc8b8ba6ac5c4b81
saphron <saphron.scul@gmail.com>
2015-11-04 09:36:42 -0500

    I think I mostly understood this assessment I only just finished it.

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,18 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, comment, patient_id, doctor_id)
+  SELECT 'August 12, 2016', '10:00 AM', 'no comment',
+    (SELECT id FROM patients WHERE surname = 'Wilcox' AND given_name = 'Marsha'),
+    (SELECT id FROM doctors WHERE surname = 'Hilton' AND given_name = 'Vicky')
+  UNION
+  SELECT 'August 17, 2016', '1:00 PM', 'no comment',
+    (SELECT id FROM patients WHERE surname = 'Talley' AND given_name = 'Ivory'),
+    (SELECT id FROM doctors WHERE surname = 'Hubbard' AND given_name = 'Spencer')
+  UNION
+  SELECT 'August 12, 2016', '10:00 AM', 'no comment',
+    (SELECT id FROM patients WHERE surname = 'Wilcox' AND given_name = 'Marsha'),
+    (SELECT id FROM doctors WHERE surname = 'Hubbard' AND given_name = 'Spencer')
+  UNION
+  SELECT 'August 17, 2016', '1:00 PM', 'no comment',
+    (SELECT id FROM patients WHERE surname = 'Hodge' AND given_name = 'Blake'),
+    (SELECT id FROM doctors WHERE surname = 'Hilton' AND given_name = 'Vicky')
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,18 @@
 -- retrieve appointment information
+SELECT p.given_name, p.surname, a.time
+FROM patients AS p
+INNER JOIN appointments AS a
+  ON a.patient_id = p.id
+INNER JOIN doctors AS d
+  ON a.doctor_id = d.id
+WHERE d.surname = 'Hubbard'
+;
+
+SELECT d.given_name, d.surname, a.time
+FROM doctors d
+INNER JOIN appointments a
+  ON a.doctor_id = d.id
+INNER JOIN patients p
+  ON a.patient_id = p.id
+WHERE p.surname = 'Wilcox' AND p.given_name = 'Marsha'
+;
