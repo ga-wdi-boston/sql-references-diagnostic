fedfa1bf3bc15ad80491acff7de1f58c1833ed8a
EdieShimel <edieshimel@gmail.com>
2015-11-04 09:37:00 -0500

    Answers to assessments

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname VARCHAR,
+  given_name VARCHAR,
+  gender VARCHAR,
+  dob INTEGER,
+  specialty VARCHAR
+);
+
+CREATE TABLE patients(
+id SERIAL PRIMARY KEY,
+surname VARCHAR
+given_name VARCHAR
+gender VARCHAR
+dob INTEGER
+insurance VARCHAR
+);
+
+CREATE TABLE appointments(
+id SERIAL PRIMARY KEY
+time TEXT,
+date TEXT,
+comment TEXT,
+patient_id INTEGER REFERENCES patients(id),
+doctor_id INTEGER) REFERENCES doctors (id)
+);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,23 @@
 -- retrieve appointment information
+--Question 4. Retrieve the names and appointment times for everyone seeing Dr. Hubbard.  Retrieve the Doctors' names and appointment times for Marsha Wilcox.
+
+SELECT p.given_name, p.surname, a.time
+FROM appointments a
+INNER JOIN patients
+  ON a.patient_id = p.id
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+
+WHERE d.appointment = 'Hubbard'
+;
+
+SELECT d.surname, p.given_name, a.time
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+INNER JOIN patients p
+  ON a.patient_id = p.id
+
+WHERE p.surname = 'Wilcox'
+AND p.given_name = 'Marsha'
+;
