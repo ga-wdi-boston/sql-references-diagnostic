417be624272d0ce4c8ea75dcf10a4365c5575fd8
kuramameng <mengjs324@gmail.com>
2015-11-04 09:35:34 -0500

    response after testing

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -12,7 +12,8 @@ INSERT INTO appointments(date,time,comment,doctor_id,patient_id)
           -- the rest of the insert assumed the known doctor and patient ids
          ('August 17, 2016','1pm','appointments',6,5),
          ('August 12, 2016','10am','appointments',6,8),
-         ('August 17, 2016','1pm','appointments',4,7),
+         ('August 17, 2016','1pm','appointments',4,7)
+;
 
 
 
