2310e5ec25e9ab48dc516497ec7a73ed3ce63ef3
Lauren Grant <lgrant1286@gmail.com>
2015-11-04 09:33:16 -0500

    Completed assessment

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,25 @@
 -- insert appointments creating connections between doctors and patients
+
+-- Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2016.
+INSERT into appointments(date, time, comment)
+SELECT 'August 12, 2016', '10am', 'seeing Dr. Vicky Hilton'
+FROM patients
+WHERE surname = 'Wilcox' AND given_name = 'Marsha';
+
+-- Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+INSERT into appointments(date, time, comment)
+SELECT 'August 17, 2016', '1pm', ' seeing Dr. Spencer Hubbard'
+FROM patients
+WHERE surname = 'Talley' AND given_name = 'Ivory';
+
+-- Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+INSERT into appointments(date, time, comment)
+SELECT 'August 12, 2016', '10am', 'seeing Dr. Spencer Hubbard'
+FROM patients
+WHERE surname = 'Wilcox' AND given_name = 'Marsha';
+
+-- Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
+INSERT into appointments(date, time, comment)
+SELECT 'August 17, 2016', '1pm',  'seeing Dr. Vicky Hilton'
+FROM patients
+WHERE surname = 'Hodge' AND given_name = 'Blake;'
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,23 @@
 -- retrieve appointment information
+
+-- Retrieve the names and appointment times for everyone seeing Dr. Hubbard.
+
+SELECT p.given_name, p.surname, a.time
+FROM patients p
+INNER JOIN appointments a
+  ON a.patient_id = p.id
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE d.surname = 'Hubbard'
+;
+
+-- Retrieve the Doctors' names and appointment times for Marsha Wilcox.
+
+SELECT d.given_name, d.surname, a.time
+FROM doctors d
+INNER JOIN appointments a
+  ON a.patient_id = p.id
+INNER JOIN patients p
+  ON a.doctor_id = d.id
+WHERE p.surname = 'Wilcox'
+;
