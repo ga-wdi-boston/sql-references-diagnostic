36a525b316951abab4baf7301bb2935fabb0ac4d
sdavidson140 <sdavidson140@gmail.com>
2015-11-04 09:32:35 -0500

    completed assessment
    didn't use built in tests, but create a db and checked code every step of the way
    and executed code behaved as expected

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
 -- insert appointments creating connections between doctors and patients
+insert into appointments(patient_id, doctor_id, date, time, comment)
+  values (8, 4,'August 12, 2016', '10am', ''),
+          (5, 6, 'August 17, 2016', '1pm', ''),
+          (8, 6, 'August 12, 2016', '10am', ''),
+          (7, 4, 'August 17, 2016', '1pm', '')
+          ;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,19 @@
 -- retrieve appointment information
+
+select p.given_name, p.surname, a.time
+from patients p
+inner join appointments a
+  on a.patient_id = p.id
+inner join doctors d
+  on a.doctor_id = d.id
+where d.surname = 'Hubbard'
+;
+
+select d.given_name, d.surname, a.time
+from doctors d
+inner join appointments a
+  on a.doctor_id = d.id
+inner join patients p
+  on a.patient_id = p.id
+where p.surname = 'Wilcox' and p.given_name = 'Marsha'
+;
