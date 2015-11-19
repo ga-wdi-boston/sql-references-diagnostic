75023f1ce6c569f2b29bd7440a5bf0c283d624c3
francescadal <francescadalembert@icloud.com>
2015-11-19 09:17:28 -0500

    Finished past due assessment went back to look at sal-join table assessment and internet searched

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,19 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments(date, time, doctor_id, patient_id)
+  SELECT '20150812', '1000',
+    (SELECT id FROM doctors WHERE surname = 'Hilton'),
+    (SELECT id FROM patients WHERE surname = 'Wilcox')
+  UNION
+  SELECT '20150817', '1300',
+    (SELECT id FROM doctors WHERE surname = 'Hubbard'),
+    (SELECT id FROM patients WHERE surname = 'Talley')
+  UNION
+  SELECT '20150817', '1000',
+    (SELECT id FROM doctors WHERE surname = 'Hubbard'),
+    (SELECT id FROM patients WHERE surname = 'Wilcox')
+  UNION
+  SELECT '20150817', '1300',
+   (SELECT id FROM doctors WHERE surname = 'Hilton'),
+   (SELECT id FROM patients WHERE surname = 'Hodge')
+;
