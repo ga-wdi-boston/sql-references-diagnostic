829333340bf6f4945b394e935b59f8d54ccff377
Celena T <chibiynm@gmail.com>
2016-02-23 09:28:48 -0500

    Add response to SQL diagnostic

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,29 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  surname VARCHAR,
+  given_name VARCHAR,
+  gender VARCHAR,
+  born_on DATE,
+  specialty VARCHAR
+);
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  surname VARCHAR,
+  given_name VARCHAR,
+  gender VARCHAR,
+  born_on DATE,
+  insurance VARCHAR
+);
+
+CREATE TABLE appointments (
+  id SERIAL PRIMARY KEY,
+  patient_surname VARCHAR,
+  patient_given_name VARCHAR,
+  doctor VARCHAR,
+  appt_date DATE,
+  appt_time TEXT,
+  comment TEXT
+)
+;
