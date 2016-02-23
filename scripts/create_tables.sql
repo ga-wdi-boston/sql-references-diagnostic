4e3b266f5414ac1a72b2490652b2b2380dff39ef
mfbrewster <michael.f.brewster@gmail.com>
2016-02-23 09:27:56 -0500

    diagnostic response

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname VARCHAR,
+  given_name VARCHAR,
+  gender VARCHAR,
+  born_on DATE,
+  specialty VARCHAR
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname VARCHAR,
+  given_name VARCHAR,
+  gender VARCHAR,
+  born_on DATE,
+  insurance VARCHAR
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  apt_date TEXT,
+  apt_time TEXT,
+  comment TEXT,
+  doctor_id INTEGER REFERENCES doctors,
+  patient_id INTEGER REFERENCES patients
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,21 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(apt_time, apt_date, patient_id, doctor_id)
+  SELECT 'August 12, 2016',
+    '10AM',
+    (SELECT id FROM patients WHERE surname = 'Wilcox' AND given_name = 'Marsha'),
+    (SELECT id FROM doctors WHERE surname = 'Hilton' AND given_name = 'Vickie')
+  UNION
+  SELECT 'August 17, 2016',
+    '1PM',
+    (SELECT id FROM patients WHERE surname = 'Talley' AND given_name = 'Ivory'),
+    (SELECT id FROM doctors WHERE surname = 'Hubbard' AND given_name = 'Spencer')
+  UNION
+  SELECT 'August 12, 2016',
+    '10AM',
+    (SELECT id FROM patients WHERE surname = 'Wilcox' AND given_name = 'Marsha'),
+    (SELECT id FROM doctors WHERE surname = 'Hubbard' AND given_name = 'Spencer')
+  UNION
+  SELECT 'August 17, 2016',
+    '1PM',
+    (SELECT id FROM patients WHERE surname = 'Hodge' AND given_name = 'Blake'),
+    (SELECT id FROM doctors WHERE surname = 'Hilton' AND given_name = 'Vickie')
