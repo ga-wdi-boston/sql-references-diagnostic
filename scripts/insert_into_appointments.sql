03290858990b77e909deb8e29206886fcbc175ff
khanhnhat <nguyen.k21@gmail.com>
2016-02-23 09:29:10 -0500

    wrote answers

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,5 @@
 -- insert appointments creating connections between doctors and patients
+-- Marsha Wilcox seeing Dr. Vickie Hilton at 10am on August 12, 2016.
+-- Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+-- Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+-- Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
