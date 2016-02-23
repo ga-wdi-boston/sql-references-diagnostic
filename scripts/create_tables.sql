5c13157d7c44db4b5d96456384e3f2511b59686e
Jeremiah <jeremiahdamiron@gmail.com>
2016-02-23 09:29:08 -0500

    response to diagnostic

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  surname VARCHAR,
+  given_name VARCHAR,
+  gender VARCHAR,
+  born_on DATE,
+  specialty VARCHAR
+);
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  surname VARCHAR,
+  given_name VARCHAR,
+  gender VARCHAR,
+  born_on DATE,
+  insurance VARCHAR
+);
+
+CREATE TABLE appointments (
+  id SERIAL PRIMARY KEY,
+  message TEXT,
+  dates TEXT,
+  times TEXT,
+  doctor_id INTEGER REFERENCES doctors,
+  patient_id INTEGER REFERENCES patients
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,10 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments (message,dates,times,doctor_id,patient_id)
+  SELECT 'Marsha Wilcox seeing Dr. Vicky Hilton',
+    (SELECT doctor_id FROM doctors WHERE given_name = 'Vickie' AND surname = 'Hilton'),
+    (SELECT patient_id FROM patient WHERE given_name = 'Marsha' AND surname = 'Wilcox'),
+  UNION
+  SELECT 'Ivory Talley seeing Dr. Spencer Hubbard.',
+  (SELECT doctor_id FROM doctors WHERE given_name = 'Spencer' AND surname = 'Hubbard'),
+  (SELECT patient_id FROM patient WHERE given_name = 'Ivory' AND surname = 'Talley'),
+;
