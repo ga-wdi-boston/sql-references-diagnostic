225801008918fb46404c786b005a65580df2a718
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-22 23:38:31 -0500

    Change indenting in 'select_appointments'
    Lined up FROM and INNER JOIN clauses,
    and brought ON clauses inline with INNER JOIN.

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,17 +1,13 @@
 -- retrieve appointment information
 -- for Dr. Hubbard
 SELECT p.surname, a.date, a.time
-FROM patients p
-INNER JOIN appointments a
-  ON a.patient_id = p.id
-INNER JOIN doctors d
-  ON a.doctor_id = d.id
-WHERE d.surname = 'Hubbard';
+  FROM patients p
+  INNER JOIN appointments a ON a.patient_id = p.id
+  INNER JOIN doctors d ON a.doctor_id = d.id
+    WHERE d.surname = 'Hubbard';
 -- for patient Wilcox
 SELECT d.surname, a.date, a.time
-FROM doctors d
-INNER JOIN appointments a
-  ON a.doctor_id = d.id
-INNER JOIN patients p
-  ON a.patient_id = p.id
-WHERE p.surname = 'Wilcox';
+  FROM doctors d
+  INNER JOIN appointments a ON a.doctor_id = d.id
+  INNER JOIN patients p ON a.patient_id = p.id
+    WHERE p.surname = 'Wilcox';
