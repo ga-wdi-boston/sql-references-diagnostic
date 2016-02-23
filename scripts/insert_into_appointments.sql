c90f4e341144a3e043a9a3cd0a95bb7ba32263aa
kpie89 <kpie89@gmail.com>
2016-02-23 09:29:09 -0500

    add answers to diagnostic

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,20 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date_of, time_of, doctor_id, patient_id)
+  SELECT '20150812', '1000',
+    (SELECT id FROM doctors WHERE surname = 'Hilton'),
+    (SELECT id FROM patients WHERE surname = 'Wilcox')
+
+INSERT INTO appointments(date_of, time_of, doctor_id, patient_id)
+  SELECT '20150817', '1300',
+    (SELECT id FROM doctors WHERE surname = 'Hubbard'),
+    (SELECT id FROM patients WHERE surname = 'Tally')
+
+INSERT INTO appointments(date_of, time_of, doctor_id, patient_id)
+  SELECT '20150812', '1000',
+    (SELECT id FROM doctors WHERE surname = 'Hubbard'),
+    (SELECT id FROM patients WHERE surname = 'Wilcox')
+
+INSERT INTO appointments(date_of, time_of, doctor_id, patient_id)
+  SELECT '20150812', '1300',
+    (SELECT id FROM doctors WHERE surname = 'Hilton'),
+    (SELECT id FROM patients WHERE surname = 'Hodge')
