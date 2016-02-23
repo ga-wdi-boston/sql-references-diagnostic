1ca43cc072e4593acff3dc34e7cdb5927935aead
ajordon <airjordon2@gmail.com>
2016-02-23 09:29:35 -0500

    ajordon diag response

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(day_scheduled, time_scheduled, with_doctor, for_patient, comments)
+VALUES('August 12, 2016', '10am', 'Dr. Vickie Hilton','Marsha Wilcox', 'Marsha Wilcox seeing Dr. Vicky Hilton'),
+      ('August 17, 2016', '1pm', 'Dr. Spencer Hubbard','Ivory Talley','Ivory Talley seeing Dr. Spencer Hubbard'),
+      ('August 12, 2016', '10am', 'Dr. Spencer Hubbard', 'Marsha Wilcox','Marsha Wilcox seeing Dr. Spencer Hubbard'),
+      ('August 17, 2016', '1pm', 'Dr. Vickie Hilton','Blake Hodge', 'Blake Hodge seeing Dr. Vicky Hilton')
+;
