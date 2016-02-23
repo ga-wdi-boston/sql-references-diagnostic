a36ff8f903e26b406139cf34882a0f4f8e72033d
RobertRuttenberg <rdruttenberg@gmail.com>
2016-02-23 09:32:52 -0500

    response

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,9 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(patient_id, doctor_id, time, date)
+(SELECT id FROM patients WHERE given_name = 'Marsha' AND surname = 'Wilcox'),
+(SELECT id FROM doctors WHERE given_name = 'Vickie' AND surname = 'Wilxcox'),
+SELECT '10am', 'August 12, 2016'
+
+UNION
+
+(SELECT id FROM patients WHERE)
