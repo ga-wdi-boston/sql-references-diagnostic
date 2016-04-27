5d6ae0d9521a0f05e19241f9e0e5e75525ebd2b1
sjf125 <sjf125@gmail.com>
2016-04-27 09:28:28 -0400

    Complete sql-references-diagnostic

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(appt_date,appt_time,comment,doctor_id,patient_id)
+VALUES('08-12-2016', '10:00', , 4, 8),
+      ('08-17-2016', '13:00', , 6, 5),
+      ('08-12-2016', '10:00', , 6, 8),
+      ('08-17-2016', '13:00', , 4, 7)
+;
