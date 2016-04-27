be029973a8433496507b1b0902d5cfc984cba7ba
philschoof <phil.schoof@gmail.com>
2016-04-27 09:21:44 -0400

    Complete sql-refernces diagnostic

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname CHARACTER VARYING,
+  given_name CHARACTER VARYING,
+  gender CHARACTER VARYING(1),
+  born_on DATE,
+  specialty CHARACTER VARYING
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname CHARACTER VARYING,
+  given_name CHARACTER VARYING,
+  gender CHARACTER VARYING(1),
+  born_on DATE,
+  insurance CHARACTER VARYING
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  date TEXT,
+  time TEXT,
+  comment TEXT,
+  doctor_id INTEGER REFERENCES doctors,
+  patient_id INTEGER REFERENCES patients
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,12 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+VALUES('August 12, 2016', '10am', 'comment', 4, 8);
+
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+VALUES('August 17, 2016', '1pm', 'comment', 6, 6);
+
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+VALUES('August 12, 2016', '10am', 'comment', 6, 8);
+
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+VALUES('August 17, 2016', '1pm', 'comment', 7, 4);
