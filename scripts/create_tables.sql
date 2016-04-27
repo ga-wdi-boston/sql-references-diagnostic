3e658882f5ce3360830214550d589c638573415b
“Benjamski” <your_email@example.com>
2016-04-27 09:30:36 -0400

    sql references diagnsotic
    comfort 3
    
    clarity 5

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE patients (
+id SERIAL PRIMARY KEY,
+surname TEXT,
+given_name TEXT,
+gender TEXT,
+born_on DATE,
+insurance TEXT,
+);
+
+CREATE TABLE doctors (
+id SERIAL PRIMARY KEY,
+surname TEXT,
+given_name TEXT,
+gender TEXT,
+born_on DATE,
+specialty TEXT,
+);
+
+CREATE TABLE appointments (
+id SERIAL PRIMARY KEY,
+appointment_date DATE,
+appointment_time TEXT,   --should this be string of time or should it be using TIME
+comment TEXT,
+patient TEXT
+doctor TEXT
+);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,11 @@
 -- retrieve appointment information
+
+SELECT COUNT(*), appointments
+FROM doctors
+GROUP BY Dr. Spencer Hubbard
+;
+
+-- retrieve the names and appointment times for everyone seeing Dr. Hubbard.
+
+
+-- retrieve the Doctors' names and appointment times for Marsha Wilcox.
