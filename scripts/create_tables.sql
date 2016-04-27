b31cf1f208f4d3072df18512620224e0b5fd3bee
susandaiute <susandaiute@gmail.com>
2016-04-27 09:28:46 -0400

    Create SQL tables, etc.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,29 @@
 -- create doctors, patients, and appointments tables
+
+create table doctors (
+  id serial primary key,
+  surname character varying,
+  given_name character varying,
+  gender character varying,
+  born_on date,
+  specialty character varying
+);
+
+
+create table patients (
+  id serial primary key,
+  surname character varying,
+  given_name character varying,
+  gender character varying,
+  born_on date,
+  insurance character varying
+);
+
+create table appointments (
+  id serial primary key,
+  day text,
+  time_of_day text,
+  comment text,
+  doctor_id integer references doctors,
+  patient_id integer references patients
+);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,15 @@
 -- retrieve appointment information
+
+select p.given_name, p.surname
+  from patients p
+  where doctor_id = 6
+  inner join appointments a
+on p.id = a.id
+;
+
+select a.day, a.time_of_day, a.doctor_id
+  from appointments a
+  where patient_id = 8
+  inner join appointments a
+on p.id = a.id
+;
