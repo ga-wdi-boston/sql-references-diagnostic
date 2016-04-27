c5499fa888be1290a2610428cc3aacb9f221ba93
V4Vince <vince.guoo@gmail.com>
2016-04-27 09:26:46 -0400

    answers

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,9 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(patients_id,doctors_id,times,dates,comment,)
+VALUES(9,5,'10AM','AUGUST 12, 2016');
+INSERT INTO appointments(patients_id,doctors_id,times,dates,comment,)
+VALUES(6,7,'1PM','AUGUST 17, 2016');
+INSERT INTO appointments(patients_id,doctors_id,times,dates,comment,)
+VALUES(9,7,'10AM','AUGUST 12, 2016');
+INSERT INTO appointments(patients_id,doctors_id,times,dates,comment,)
+VALUES(8,5,'1PM','AUGUST 12, 2016');
