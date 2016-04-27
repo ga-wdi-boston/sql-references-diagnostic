28f4c14711b6dbdac51462c1f064776b64f3c6a8
soynog <nicolas.o.garcia@gmail.com>
2016-04-27 09:28:50 -0400

    SQL References Diagnostic Response

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments (appt_date, appt_time, comment, doctor_id, patient_id)
+  VALUES ('2016-08-12', '10:00', 'bad rash', 4, 8),
+         ('2016-08-17', '13:00', 'eye ache', 6, 5),
+         ('2016-08-12', '10:00', 'hair turning purple', 6, 8),
+         ('2016-08-17', '13:00', 'wow I did not know you could get a parasite there', 4, 7)
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,12 @@
 -- retrieve appointment information
+SELECT p.given_name, p.surname, a.appt_time
+  FROM patients p
+  INNER JOIN appointments a
+ON a.doctor_id = 6 AND a.patient_id = p.id
+;
+
+SELECT d.given_name, d.surname, a.appt_time
+  FROM doctors d
+  INNER JOIN appointments a
+ON a.doctor_id = d.id AND a.patient_id = 8
+;
