ff526387beea14b029f61b951f3104e6d8a0c383
rdelvallej32 <rdelvallej@gmail.com>
2016-04-27 09:26:38 -0400

    response to diagnostic
    comfort 2
    clarity 3

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,16 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(app_date, app_time , doctor_id, patient_id)
+  VALUES ('2016-08-12','10AM',4, 8)
+;
+
+INSERT INTO appointments(app_date, app_time , doctor_id, patient_id)
+  VALUES ('2016-08-17','1PM',6, 5)
+;
+
+INSERT INTO appointments(app_date, app_time , doctor_id, patient_id)
+  VALUES ('2016-08-12','10AM',6, 8)
+;
+
+INSERT INTO appointments(app_date, app_time , doctor_id, patient_id)
+  VALUES ('2016-08-17','1PM',4, 7)
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,6 @@
 -- retrieve appointment information
+SELECT p.given_name, p.surname,
+  from patients p
+  INNER JOIN appointments a
+  on p.id = 6
+  ;
