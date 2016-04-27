b66319862feb164a3b82f9a38c9359991462462a
OllieBuilds <scarborough.oliver@gmail.com>
2016-04-27 09:26:19 -0400

    Answers all diagnostic questions
    I should have read through the diagnostic first...
    
    In my haste I created the appointments table in a manner that made
    the following queries very difficult. For select_appointments.sql, let's just \
    pretend I built the table to work.

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,11 @@
--- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date,time,comment)
+VALUES ('2016-08-12','10am','Marsha Wilcox seeing Dr. Vicky Hilton');
+
+INSERT INTO appointments(date,time,comment)
+VALUES ('2016-08-17','1pm','Ivory Talley seeing Dr. Spencer Hubbard');
+
+INSERT INTO appointments(date,time,comment)
+VALUES ('2016-08-12','10am','Marsha Wilcox seeing Dr. Spencer Hubbard')
+
+INSERT INTO appointments(date,time,comment)
+VALUES ('2016-08-17','1pm','Blake Hodge seeing Dr. Vicky Hilton');
