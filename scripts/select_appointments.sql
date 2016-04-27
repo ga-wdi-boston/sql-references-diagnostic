6437499a0aba07038bfb33880750af3f13e7e791
natdjerf <nadjerf@gmail.com>
2016-04-27 09:29:15 -0400

    Response to sql diagnostic.

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,16 @@
 -- retrieve appointment information
+
+
+select p.given_name, p.surname
+  from patients p
+  inner join appointments a
+  on p.id = a.id
+;
+
+--
+--
+-- Write a query to
+--  retrieve the names and appointment times for everyone seeing Dr. Hubbard.
+-- Then, write another to
+--  retrieve the Doctors' names and appointment times for Marsha Wilcox.
+-- Save your code in `scripts/select_appointments.sql`.
