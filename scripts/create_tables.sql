6964732557917f322ea577267f0547909f849789
logicmyth <staley77@protonmail.com>
2016-06-22 09:31:09 -0400

    DIAG
    Answer q's

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,4 @@
 -- create doctors, patients, and appointments tables
+CREATE DATABASE "doctors";
+ALTER TABLE doctors ADD (surname varchar,given_name varchar,gender varchar,born_on date,specialty varchar);
+)
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,3 @@
 -- insert appointments creating connections between doctors and patients
+COPY doctors FROM `data/doctors.csv` HEADERS true;
+COPY patients FROM `data/doctors.csv` HEADERS true;
