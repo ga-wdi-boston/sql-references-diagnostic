0501d7e581a3969db58521c80746e9c09035b73f
tina513 <dlnaz.ai@hotmail.com>
2016-06-22 09:34:15 -0400

    second commit

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,8 +1,20 @@
 -- retrieve appointment information
 
 
-select
+select p.surname, p.given_name, a.time
   from appointments a
   inner join doctors d
-  on 
+  on d.id = a.doctor_id
+  inner join patients p
+  on d.id = a.patient_id
+  where d.id=7
+;
+
+select d.surname, d.given_name, a.time
+  from appointments a
+  inner join doctors d
+  on d.id = a.doctor_id
+  inner join patients p
+  on d.id = a.patient_id
+  where p.id=9
 ;
