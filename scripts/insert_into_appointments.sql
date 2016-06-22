8c103d29fb37cdb5dc241f848a258c80368c8220
mattb5 <mattbuffardi@gmail.com>
2016-06-22 09:28:42 -0400

    diagnostic responses -- with another 10 minutes..
    could have finished the last question

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,8 @@
 -- insert appointments creating connections between doctors and patients
+insert into appointments( appointment_date, apointment_time, comments, doctor, patient) values ( '2016-08-12', '10AM', 'no comments', 4,8);
+
+insert into appointments( appointment_date, apointment_time, comments, doctor, patient) values ( '2016-08-17', '1pm', 'no comments', 6,5);
+
+insert into appointments( appointment_date, apointment_time, comments, doctor, patient) values ( '2016-08-12', '10AM', 'no comments', 6,8);
+
+insert into appointments( appointment_date, apointment_time, comments, doctor, patient) values ( '2016-08-17', '1pm', 'no comments', 4,7);
