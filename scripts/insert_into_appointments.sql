966a8340fa67ea97be50b9e26d4056b212667618
erinsul <erin_sullivan50@yahoo.com>
2016-06-22 09:22:40 -0400

    Answer all questions
    Answered all questions

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,17 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+  VALUES ('2016-08-12', '10am', '', 4, 8)
+;
+
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+  VALUES ('2016-08-17', '1pm', '', 6, 5)
+;
+
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+  VALUES ('2016-08-12', '10am', '', 6, 8)
+--Marsha literally has two appointments at the same time, so this dr office kinda sucks at organization
+;
+
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+  VALUES ('2016-08-17', '1pm', '', 4, 7)
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,16 @@
 -- retrieve appointment information
+
+SELECT p.given_name, p.surname, a.date, a.time
+FROM patients as p
+INNER JOIN appointments as a
+ON p.id=a.patient_id;
+WHERE a.doctor_id=6
+--everyone seeing dr hubbard
+
+
+SELECT d.given_name, d.surname, a.date, a.time
+FROM doctors as d
+INNER JOIN appointments as a
+ON d.id=a.doctor_id;
+WHERE a.patient_id=8
+--Marsha's fail appointments
