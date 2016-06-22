fc613a4ad4e2d3d84f2b5c6eca7a6d4e87f44285
shkherad <shkherad@bu.edu>
2016-06-22 09:29:58 -0400

    done
    done

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname varchar,
+  given_name varchar,
+  gender varchar,
+  born_on date,
+  specialty varchar
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname varchar,
+  given_name varchar,
+  gender varchar,
+  born_on date,
+  insurance varchar
+);
+
+CREATE TABLE appointments(
+  date text,
+  time text,
+  comment text,
+  doctor integer REFERENCES doctors,
+  patient integer REFERENCES patients
+)
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,8 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments(date,time,comment,doctor,patient)
+VALUES
+('August 12, 2016','10am',null, 4, 8),
+('August 17, 2016','1pm',null, 6, 5),
+('August 12, 2016','10am',null, 6, 8),
+('August 17, 2016','1pm',null, 4, 7);
