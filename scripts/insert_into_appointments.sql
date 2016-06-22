251b685783738a9d8ffb249f45c37e4ee03d523f
MishaHerscu <mishaherscu@gmail.com>
2016-06-22 09:31:59 -0400

    question 3

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,13 @@
 -- insert appointments creating connections between doctors and patients
+
+-- Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2016.
+-- Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+-- Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+-- Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
+
+INSERT INTO appointments (id, date, time, comment, doctor_id, patient_id)
+VALUES
+('2016-08-12', '10:00', '', 4, 8),
+('2016-08-17', '13:00', '', 6, 5),
+('2016-08-12', '10:00', '', 6, 8),
+('2016-08-17', '13:00', '', 4, 7);
