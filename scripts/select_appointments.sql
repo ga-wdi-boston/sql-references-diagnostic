275a03eb216b7fcc8e671f4126e0e371b1fa50c2
SarahHartwick <sarah@surfsetfitness.com>
2016-06-22 09:26:27 -0400

    SQL References Diagnostic
    Answer all questions

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,23 @@
 -- retrieve appointment information
+
+-- Write a query to retrieve the names and appointment times for everyone seeing
+-- Dr. Hubbard
+
+SELECT p.surname, p.given_name, a.day_on, a.time_at
+FROM appointments as a
+INNER JOIN doctors as d
+ON d.id = a.doctor_id
+INNER JOIN patients as p
+ON p.id = a.patient_id
+WHERE d.surname = 'Hubbard';
+
+-- Then, write another to retrieve the Doctors' names and appointment times for
+-- Marsha Wilcox
+
+SELECT d.surname, d.given_name, a.day_on, a.time_at
+FROM appointments as a
+INNER JOIN doctors as d
+ON d.id = a.doctor_id
+INNER JOIN patients as p
+ON p.id = a.patient_id
+WHERE p.surname = 'Wilcox';
