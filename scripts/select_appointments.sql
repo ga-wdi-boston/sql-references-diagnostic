41940f70752155f0cac1194d37c93ac8611919d1
andreasantarl <andrea.santarl@gmail.com>
2016-06-22 09:26:58 -0400

    Responses to SQL References Diagnostic
    Loading files didn't work.  I used the command in the load_doctors_and_patients.psql file and
    kept getting an error below (command on line 6, error on line 8)
    
    \copy doctors(surname, given_name, gender, born_on, specialty) FROM 'data/doctors.csv’ WITH (HEADER true, FORMAT csv)
    
    data/doctors.csv’ WITH (HEADER true, FORMAT csv): No such file or directory

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,20 @@
 -- retrieve appointment information
+
+-- Write a query to
+--  retrieve the names and appointment times for everyone seeing Dr. Hubbard.
+
+ SELECT p.names AS names, a.appt_date AS appt_date
+ FROM patients AS p
+  INNER JOIN appointments AS a
+  ON p.id = a.patient_id
+ WHERE a.doctor_id = 6;
+
+
+-- Then, write another to
+--  retrieve the Doctors' names and appointment times for Marsha Wilcox.
+
+ SELECT d.names AS doctor_names, a.appt_times AS appointment_times
+ FROM doctors AS d
+ INNER JOIN appointments AS a
+  ON d.id = a.doctor_id
+ WHERE a.patient_id = 8;
