fb06301ac7a515167f37bf5c43c90ec3b630e91a
huey1021 <huey1021@aol.com>
2016-06-22 09:29:07 -0400

    response
    psql

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments (patient, doctor, time_of, date_of)
+  VALUES ('Marsha Wilcox', 'Dr. Vicky Hilton', '10am', '2016-8-12')
+         ('Ivory Talley', 'Dr. Spencer Hubbard', '1pm', '2016-8-17')
+         ('Marsha Wilcox', 'Dr. Spencer Hubbard', '10am', '2016-8-12')
+         ('Blake Hodge', 'Dr. Vicky Hilton', '1pm', '2016-8-17')
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,3 @@
 -- retrieve appointment information
+SELECT * FROM appointments WHERE doctor = 'Dr. Hubbard';
+SELECT * FROM appointments WHERE patient = 'Marsha Wilcox' GROUP BY doctor AND time_of;
