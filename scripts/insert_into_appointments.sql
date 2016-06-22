81b158b376bc3bec1d2a47bcc876b4ef85a7fa82
MishaHerscu <mishaherscu@gmail.com>
2016-06-22 09:32:47 -0400

    edit q3

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -5,7 +5,7 @@
 -- Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
 -- Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
 
-INSERT INTO appointments (id, date, time, comment, doctor_id, patient_id)
+INSERT INTO appointments (date, time, comment, doctor_id, patient_id)
 VALUES
 ('2016-08-12', '10:00', '', 4, 8),
 ('2016-08-17', '13:00', '', 6, 5),
