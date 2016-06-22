64b2474b34bea1ce0f52f67584a37425d0c77e58
MishaHerscu <mishaherscu@gmail.com>
2016-06-22 09:17:02 -0400

    first question

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,31 @@
 -- create doctors, patients, and appointments tables
+
+-- doctors table
+CREATE TABLE doctors (
+id SERIAL PRIMARY KEY,
+surname varchar,
+given_name varchar,
+gender varchar,
+born_on integer,
+specialty varchar
+);
+
+-- patients table
+CREATE TABLE patients (
+id SERIAL PRIMARY KEY,
+surname varchar,
+given_name varchar,
+gender varchar,
+born_on integer,
+insurance varchar
+);
+
+-- appointments table
+CREATE TABLE appointments (
+id SERIAL PRIMARY KEY,
+date TEXT,
+time TEXT,
+comment TEXT,
+doctor_id integer REFERENCES doctors,
+patient_id integer REFERENCES patients
+);
