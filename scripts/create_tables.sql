67f9e2e6955e1c86763912af5fb2dc3494236708
RoobScoobs <iruben229@gmail.com>
2016-06-22 09:23:33 -0400

    Sql-references diagnostic
    Update scripts with answers

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,28 @@
 -- create doctors, patients, and appointments tables
+
+CREATE TABLE doctors (
+  id serial primary key
+  surname varchar,
+  given_name varchar,
+  gender varchar,
+  born_on date,
+  specialty varchar
+);
+
+CREATE TABLE patients (
+  id serial primary key
+  surname varchar,
+  given_name varchar,
+  gender varchar,
+  born_on date,
+  insurance varchar
+);
+
+CREATE TABLE appointments (
+  id serial primary key
+  doctor_id integer,
+  patient_id integer,
+  appointment_date text,
+  appointment_time text,
+  comment text
+);
