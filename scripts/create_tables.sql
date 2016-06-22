f5fe730af27fc77ed5bb361e0b572471d5c7401d
modularjon <modularjon@gmail.com>
2016-06-22 09:28:11 -0400

    Jon J sql references diagnostic response

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,29 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  surname varchar,
+  given_name varchar,
+  gender varchar,
+  born_on integer,
+  speciality varchar
+)
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  surname varchar,
+  given_name varchar,
+  gender varchar,
+  born_on integer,
+  insurance varchar
+)
+
+-- I've appended appointment_ to the column names in this table to avoid
+-- confusion.
+CREATE TABLE appointments (
+  id SERIAL PRIMARY KEY,
+  appointment_date TEXT,
+  appointent_time TEXT,
+  comment TEXT,
+  doctor_id integer REFERENCES doctors,
+  patient_id integer REFERENCES patients
+)
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,4 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(patient_id, doctor_id, appointent_time, appointment_date)
+VALUES (8, 4, '10am', 'August 12, 2016'), (5, 6, '1pm', 'August 17, 2016'),
+       (8, 6, '10am', 'August 12, 2016'), (7, 4, '1pm', 'August 17, 2016')
