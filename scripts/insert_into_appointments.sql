ab0d9088e2df5dc0b9862907b942205727f93fe3
lcurran <lcurran327@gmail.com>
2016-06-22 09:25:27 -0400

    Diagnostic complete

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,11 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments (doctor_id, patient_id, date, time) VALUES (4, 8, '2016-08-12', '10:00');
+
+
+INSERT INTO appointments (doctor_id, patient_id, date, time) VALUES (6, 5, '2016-08-17', '13:00');
+
+INSERT INTO appointments (doctor_id, patient_id, date, time) VALUES (6, 8, '2016-08-12', '10:00');
+
+
+INSERT INTO appointments (doctor_id, patient_id, date, time) VALUES (4, 7, '2016-08-17', '13:00');
