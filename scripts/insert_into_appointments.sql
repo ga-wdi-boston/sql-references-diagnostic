5c7b884ae997d895f08f973ba5d70895fdcd22fb
cdelsoin <cdelsoin@gmail.com>
2016-08-12 09:28:05 -0400

    answered questions
    i just realized there were specific patients for #3..

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,4 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(start,comments,patient_id,doctor_id) VALUES
+  ('2016-09-22 11:30', smells bad, 1, 1),
+  ('2016-09-01 11:59', always asks for percocet, 5, 3);
