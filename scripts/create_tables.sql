06b3fe751b6e25a1916450d0e9617a427827bed3
EchoXin <xin.yi@husky.neu.edu>
2016-08-12 09:27:57 -0400

    Add response

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on TEXT,
+  specialty TEXT
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on TEXT,
+  insurance TEXT
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  start_date TEXT,
+  start_time TEXT,
+  comment TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,6 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(start_date, start_time, comment,doctor_id,patient_id) VALUES
+  ('August 12, 2016', '10am', 4, 8),
+  ('August 17, 2016', '1pm', 6, 5),
+  ('August 12, 2016', '10am', 5, 8),
+  ('August 17, 2016', '1pm', 4, 7);
