921ca1e83c401787e06f297ae02557788051454a
alex-braun <braunacb@gmail.com>
2016-08-12 09:27:15 -0400

    Diagnostic completed.
    Many questions were not completed.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,25 @@
--- create doctors, patients, and appointments tables
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  specialty TEXT
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  day TEXT,
+  start TEXT,
+  comment TEXT,
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctors(id)
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,3 @@
--- insert appointments creating connections between doctors and patients
+INSERT INTO appointments (patient_id, comment, doctor_id, start, day) VALUES
+  ( '10am', seeing, 2, 13),
+  ('2016-10-10 08:30', 30, 4, 16);
