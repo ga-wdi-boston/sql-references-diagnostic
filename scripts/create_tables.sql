3a14bc120a5d95da8213bc1ac336f9dca633062d
natGalynker <natalgal@gmail.com>
2016-08-12 09:26:27 -0400

    Completed the diagnostic
    -got held up on second to last question
    -was unsure if me pushing the doctor/patient id was enough or if their TEXT values was wanted

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,25 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY
+  surname TEXT,
+  given_name TEXT,
+  specialty TEXT,
+  gender TEXT,
+  born_on DATE
+
+);
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  insurance TEXT,
+  gender TEXT,
+  born_on DATE
+);
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  aptdate TIMESTAMP,
+  doc_comment TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
