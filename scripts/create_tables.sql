e4d236dd8601da3c679366360b077be128da88e1
jlmitch1989 <joshuamitchell@Joshuas-MacBook-Pro.local>
2016-08-12 09:27:04 -0400

    Add answers to sql-references-diagnostic
    Didn't have time to answer the last question.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  day TEXT,
+  start TEXT,
+  comments TEXT,
+  doctor_name TEXT,
+  patient_name text
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,17 @@
 -- insert appointments creating connections between doctors and patients
+### Question 3
+
+-- Create the following appointments by inserting
+--  the appropriate rows into the appointments table.
+-- Save your code in `insert_into_appointments.sql`:
+--
+-- -   Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2016.
+-- -   Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+-- -   Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+-- -   Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
+
+INSERT INTO appointments(day,start,comments,doctor_name,patient_name) VALUES
+  ('August 12, 2016', '10am', 'Has cough', 'Dr. Vicky Hilton', 'Marsha Wilcox')
+  ('August 17, 2016', '1pm', 'Upset stomach', 'Dr. Spencer Hubbard', 'Marsha Wilcox')
+  ('August 12, 2016', '10am', 'headaches', 'Dr. Vicky Hilton', 'Marsha Wilcox')
+  ('August 17, 2016', '1pm', 'back pain', 'Dr. Spencer Hubbard', 'Marsha Wilcox')
