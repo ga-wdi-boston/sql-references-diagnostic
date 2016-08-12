9858515337d74bcb9912c83b36f74c6db24b8067
NatasaPeic <peic.natasa@gmail.com>
2016-08-12 09:27:28 -0400

    Add diagnostic.md (sql-references-diagnostic)

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,16 @@
 -- insert appointments creating connections between doctors and patients
+
+--
+-- Create the following appointments by inserting the appropriate rows into the appointments table. Save your code in insert_into_appointments.sql:
+--
+-- Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2016.
+-- Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+-- Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+-- Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
+
+
+INSERT INTO appointments(date_app, time_app, comment, patient_id, doctor_id) VALUES
+('2016-08-12', '10:00', '', 9, 5),
+('2016-08-17', '01:00', '', 6, 7),
+('2016-08-12', '10:00', '', 9, 7),
+('2016-08-17', '10:00', '', 8, 5);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,11 @@
 -- retrieve appointment information
+--
+-- Write a query to retrieve the names and appointment times for everyone seeing Dr. Hubbard. Then, write another to retrieve the Doctors' names and appointment times for Marsha Wilcox. Save your code in scripts/select_appointments.sql.
+
+SELECT p.surname, p.given_name, a.date_app, a.time_app, d.surname, d.specialty
+FROM appointments a
+  INNER JOIN doctors d
+  ON d.id = a.doctor_id
+  INNER JOIN patients p
+  ON p.id = a.patient_id
+WHERE d.surname = 'Hubbard';
