800d3a82915bdfdb9dfed62c3aa0521c64da4c99
gabescarbrough <gabescarbrough@gmail.com>
2016-08-12 09:27:31 -0400

    Complete SQL References Diagnostic

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,31 @@
 -- create doctors, patients, and appointments tables
+
+-- create a table to store information about doctors
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+-- create a table to store information about patients
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+-- create a table to store information about appointments
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  appt_date TEXT,
+  appt_time TEXT,
+  comment TEXT,
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctors(id)
+);
