4170e1cb56225d2f733f9ae2c0b23ea81f65f0d9
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-27 09:24:27 -0400

    Write scripts for diagnostic
    Create tables
    Bulk load data
    Insert into appointments
    Select from appointments

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,30 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date_scheduled, start_time, doctor_id, patient_id) 
+  SELECT '2016-08-12', '10:00', d.id, p.id 
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+  UNION
+  SELECT '2016-08-17', '13:00', d.id, p.id 
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND p.given_name = 'Ivory'
+    AND p.surname = 'Talley'
+  UNION
+  SELECT '2016-08-12', '10:00', d.id, p.id 
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+  UNION
+  SELECT '2016-08-17', '13:00', d.id, p.id 
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND p.given_name = 'Blake'
+    AND p.surname = 'Hodge'
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,19 @@
 -- retrieve appointment information
+SELECT p.surname as patient, a.date_scheduled, a.start_time
+  FROM appointments a
+  INNER JOIN doctors d
+    ON a.doctor_id = d.id
+  INNER JOIN patients p
+    ON a.patient_id = p.id
+  WHERE d.surname = 'Hubbard'
+;
+
+
+SELECT d.surname as doctor, a.date_scheduled, a.start_time
+  FROM appointments a
+  INNER JOIN doctors d
+    ON a.doctor_id = d.id
+  INNER JOIN patients p
+    ON a.patient_id = p.id
+  WHERE p.surname = 'Wilcox'
+;
