b54752283840e249e08daf05570c0af8fd5f6ef6
NickBoyadjian <nboyadjian95@gmail.com>
2016-09-27 09:21:22 -0400

    response

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,8 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date,time,patient,doctor) VALUES
+  ('August 12, 2016', '10am', 'Marsha Wilcox', 'Dr. Vicky Hilton'),
+  ('August 17, 2016', '1pm', 'Ivory Talley', 'Dr. Spencer Hubbard')
+  ('August 12, 2016', '10pm', 'Marsha Wilcox', 'Dr. Spencer Hubbard')
+  ('August 17, 2016', '1pm', 'Blake Hodge', 'Dr. Vicky Hilton')
+  ;
+
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,16 @@
 -- retrieve appointment information
+SELECT p.name, d.name, a.time
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor = 'Dr. Hubbard'
+INNER JOIN patients p
+  ON a.patient_id = p.name
+;
+
+SELECT p.name, d.name, a.time
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor = d.name
+INNER JOIN patients p
+  ON a.patient_id = 'Marsha Wilcox'
+;
