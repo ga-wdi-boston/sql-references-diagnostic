71fa9ce118c05e5987bab007c7a1a2467e0730a2
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-27 09:29:14 -0400

    complete diagnostic
    complete diagnostic

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,35 @@
 -- insert appointments creating connections between doctors and patients
+-- Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2016.
+-- Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+-- Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+-- Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
+
+INSERT INTO appointments(start_date, start_time, doctor_id, patient_id)
+  SELECT 'August 12, 2016', '10am', d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+  UNION
+  SELECT 'August 17, 2016', '1pm', d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND p.given_name = 'Ivory'
+    AND p.surname = 'Talley'
+  UNION
+  SELECT 'August 12, 2016', '10am', d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+  UNION
+  SELECT 'August 17, 2016', '1pm', d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND p.given_name = 'Blake'
+    AND p.surname = 'Hodge'
+;
