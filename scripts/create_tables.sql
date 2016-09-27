33e8ceee982d0ab1d7c805bceaeec4699f5bb708
nlkassad <nlkassad@hotmail.com>
2016-09-27 09:27:48 -0400

    Add Responses to Diagnostic Code
    Add responses to code questions in the diagnostic.
    Reference material:
    https://github.com/ga-wdi-boston/sql-references-join
    https://github.com/ga-wdi-boston/rails-api-library-demo

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE appontments(
+  id SERIAL PRIMARY KEY,
+  appointment_date DATE,
+  appointment_time TIME,
+  doctor REFERENCES,
+  patient REFERENCES
+);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,5 @@
 -- retrieve appointment information
+SELECT (p.surname, p.given_name, a.date, a.time)
+FROM appointments a, patients p, doctors d
+WHERE d.surname = 'Hubbard'
+  AND d.given_name = 'Spencer'
