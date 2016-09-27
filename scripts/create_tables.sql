7e1602eda9c9c42ce82a2e016edeef5c5e134bbc
tuckyeah <h.rosebrock@gmail.com>
2016-09-27 09:21:08 -0400

    Complete diagnostic -- Fill in code for all sql scripts

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,29 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender varchar(1),
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender varchar(1),
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE appointments (
+  id SERIAL PRIMARY KEY,
+  on_date TEXT,
+  start_time TEXT,
+  comment TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
+CREATE INDEX ON appointments(doctor_id);
+CREATE INDEX ON appointments(patient_id);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,30 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(on_date, start_time, comment, doctor_id, patient_id)
+  SELECT DATE '2016-08-12', '10am', '', d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+UNION
+  SELECT DATE '2016-08-17', '1pm', '', d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND p.given_name = 'Ivory'
+    AND p.surname = 'Talley'
+UNION
+  SELECT DATE '2016-08-12', '10am', '', d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+UNION
+  SELECT DATE '2016-08-17', '1pm', '', d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND p.given_name = 'Blake'
+    AND p.surname = 'Hodge'
+;
