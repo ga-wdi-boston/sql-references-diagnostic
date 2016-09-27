114c1256349edcda8d19762652ac76bf1e781ac9
BMorse09 <benmorse09@gmail.com>
2016-09-27 09:28:02 -0400

    Provide answers for all questions

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,38 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, doctor_id, patient_id)
+  SELECT TIMESTAMP '2016-08-12 10:00', 30, d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND d.specialty = 'General practice'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+  UNION
+
+  SELECT TIMESTAMP '2016-08-17 13:00', 30, d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND d.specialty = 'Geriatrics'
+    AND p.given_name = 'Ivory'
+    AND p.surname = 'Talley'
+  UNION
+
+  SELECT TIMESTAMP '2016-08-17 10:00', 30, d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND d.specialty = 'Geriatrics'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+  UNION
+
+  SELECT TIMESTAMP '2016-08-17 13:00', 30, d.id, p.id
+  FROM doctors d, patients p
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND d.specialty = 'General practice'
+    AND p.given_name = 'Blake'
+    AND p.surname = 'Hodge'
+  UNION
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,51 @@
 -- retrieve appointment information
+
+SELECT d.surname doctor, d.specialty, p.surname patient, a.start, a.length
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE d.surname ='Hubbard'
+INNER JOIN patients p
+  ON a.patient_id = p.id
+
+SELECT d.surname doctor, d.specialty, p.surname patient, a.start, a.length
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE p.surname ='Wilcox'
+INNER JOIN patients p
+  ON a.patient_id = p.id
+;
+
+SELECT d.surname doctor, d.specialty, p.surname patient, a.start, a.length
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE d.surname ='Hilton'
+INNER JOIN patients p
+  ON a.patient_id = p.id
+
+SELECT d.surname doctor, d.specialty, p.surname patient, a.start, a.length
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE p.surname ='Hodge'
+INNER JOIN patients p
+  ON a.patient_id = p.id
+:
+
+SELECT d.surname doctor, d.specialty, p.surname patient, a.start, a.length
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE d.surname ='Hilton'
+INNER JOIN patients p
+  ON a.patient_id = p.id
+
+SELECT d.surname doctor, d.specialty, p.surname patient, a.start, a.length
+FROM appointments a
+INNER JOIN doctors d
+  ON a.doctor_id = d.id
+WHERE p.surname ='Wilcox'
+INNER JOIN patients p
+  ON a.patient_id = p.id
