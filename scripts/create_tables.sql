c35b431a8af61aab023a91e70f5502cd01f5928e
jdaly04 <julia.m.daly@gmail.com>
2016-11-18 09:28:28 -0500

    Add responses to sql-references diagnostics
    Answer questions for 11/18 diagnostic.
    Comfort: 2
    Clarity: 3

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,28 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+)
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+)
+
+CREATE TABLE appointments (
+  id SERIAL PRIMARY KEY,
+  date TEXT,
+  time TEXT,
+  comments TEXT,
+
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
