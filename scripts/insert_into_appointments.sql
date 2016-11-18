b7b3ddc828b494990a3ddda3f216468ba68e040f
treep78 <treep78@gmail.com>
2016-11-18 09:30:54 -0500

    add answers in scripts

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,30 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date_time, comments, patient_id, doctor_id)
+  SELECT TIMESTAMP '2016-08-12 10:00', '', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hamilton'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+  UNION
+  SELECT TIMESTAMP '2016-08-17 13:00', '', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND p.given_name = 'Ivory'
+    AND p.surname = 'Talley'
+  UNION
+  SELECT TIMESTAMP '2016-08-12 10:00', '', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+  UNION
+  SELECT TIMESTAMP '2016-08-17 13:00', '', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hamilton'
+    AND p.given_name = 'Blake'
+    AND p.surname = 'Hodge'
+    ;
