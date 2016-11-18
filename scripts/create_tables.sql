99f414befb8d70acfbb9e75b0f3567967c5d2c17
RpRoderick <ryanproderick@gmail.com>
2016-11-18 09:30:52 -0500

    Diagnostic Fri Nov 18
    SQL

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,29 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  start TIMESTAMP,
+  comments TEXT,
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctors(id)
+);
+
+CREATE INDEX ON appointments(patient_id);
+CREATE INDEX ON appointments(doctor_id);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,53 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(start, comments, doctor_id, patient_id)
+  SELECT TIMESTAMP '2016-08-17 13:00', 'Gross', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.specialty = 'Dermatology'
+    AND d.given_name = 'Spencer'
+    AND d.surname = 'Hubbard'
+    AND d.gender = 'm'
+
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+    AND p.born_on = '1949-08-28'
+    AND p.gender = 'f'
+    AND p.insurance = 'Blue Shield'
+UNION
+SELECT TIMESTAMP '2016-08-12 10:00', 'Flu', d.id, p.id
+    FROM doctors d, patients p
+    WHERE d.specialty = 'Internal Medicine'
+    AND d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND d.gender = 'f'
+
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+    AND p.born_on = '1949-08-28'
+    AND p.gender = 'f'
+    AND p.insurance = 'Blue Shield'
+UNION
+SELECT TIMESTAMP '2016-08-12 10:00', 'Flu', d.id, p.id
+    FROM doctors d, patients p
+    WHERE d.specialty = 'Internal Medicine'
+    AND d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND d.gender = 'f'
+
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+    AND p.born_on = '1949-08-28'
+    AND p.gender = 'f'
+    AND p.insurance = 'Blue Shield'
+UNION
+SELECT TIMESTAMP '2016-08-17 13:00', 'Something', d.id, p.id
+    FROM doctors d, patients p
+    WHERE d.specialty = 'Internal Medicine'
+    AND d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND d.gender = 'f'
+
+    AND p.given_name = 'Blake'
+    AND p.surname = 'Hodge'
+    AND p.born_on = '1960-10-22'
+    AND p.gender = 'm'
+    AND p.insurance = 'Blue Shield'
