52286025fa4bd96245a852311c147555b9acd50a
kfili <kosta.filiotis@gmail.com>
2016-11-18 09:30:44 -0500

    response

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,10 @@
 -- insert appointments creating connections between doctors and patients
+
+-   Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2016.
+-   Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+-   Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+-   Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
+
+INSERT INTO appointments()) VALUES
+  ('Rollins', 'Henry', 'm', 69, 210, '1961-02-13'),
+  ( );
