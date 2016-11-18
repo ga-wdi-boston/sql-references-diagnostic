33d824f1a52929db157df5af57317abcc3544822
kiraamaa <kira.a.maclean@gmail.com>
2016-11-18 09:23:27 -0500

    Complete sql-references diagnostic
    Not sure if everything works
    Unable to test files in psql sql-crud because of pre-existing tables
    Didn't want to drop tables and lose them if needed for next lesson

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  date TEXT,
+  time TEXT,
+  comment TEXT,
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctors(id)
+);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,14 @@
 -- retrieve appointment information
+SELECT p.surname, p.given_name, a.date, a.time
+FROM patients p
+INNER JOIN appointments a ON a.patient_id = p.id
+INNER JOIN doctors d ON a.doctor_id = d.id
+  WHERE d.surname = 'Hubbard'
+  AND d.given_name = 'Spencer';
+
+SELECT d.surname, d.given_name, a.date, a.time
+FROM doctors d
+INNER JOIN appointments a ON a.doctor_id = d.id
+INNER JOIN patients p ON a.patient_id = p.id
+  WHERE p.surname = 'Wilcox'
+  AND p.given_name = 'Marsha';
