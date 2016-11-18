34604d9aef681831dd5114bf9f7e1d9e1e201f26
callmelife <matthew.g.sommers@gmail.com>
2016-11-18 09:27:40 -0500

    Submitting diagnostic.
    Comfort: 4/5
    Clarity 4/5

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,10 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(pasurname,given_name,gender,height,weight,born_on) VALUES
+  ('Rollins', 'Henry', 'm', 69, 210, '1961-02-13'),
+  ('Smith', 'Patti', 'f', 68, 110, '1947-12-30');
+
+INSERT INTO appointments(patient_given_name, patient_surname, doctor_given_name, doctor_surname, appt_time, date_of) VALUES
+  ('Marsha', 'Wilcox', 'Dr. Vicky', 'Hilton', '10:00am', '2016-08-12'),
+  ('Ivory', 'Tailey', 'Dr. Spencer', 'Hubbard', '01:00pm', '2016-08-17'),
+  ('Marsha', 'Wilcox', 'Dr. Spencer', 'Hubbard', '10:00am', '2016-08-12'),
+  ('Blake', 'Hodge', 'Dr. Vicky', 'Hilton', '01:00pm', '2016-08-17');
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,8 @@
 -- retrieve appointment information
+SELECT COUNT(*), a.patient_given_name, a.patient_surname
+FROM appointments a
+INNER JOIN doctors d
+  ON p.doctor_id = d.id
+WHERE d.specialty <> 'General practice'
+GROUP BY d.specialty
+ORDER BY COUNT(*) ASC;
