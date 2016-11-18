14af2f116fb80a7749c9c7f0aad163f200d4023b
Naki21 <Yianni21@gmail.com>
2016-11-18 09:28:50 -0500

    Responds to SQL References diagnostis

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,8 @@
--- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, comments)
+SELECT 'August 12, 2016', '10AM', p.id, d.id
+FROM patients p, doctors d
+  WHERE d.given_name = 'Vicky'
+    AND d.surname = 'Hilton'
+    AND p.given_name = 'Marsha'
+    AND p.surname = 'Wilcox'
+;
