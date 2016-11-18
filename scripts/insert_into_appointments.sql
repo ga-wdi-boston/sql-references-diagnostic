16c6fff59ea4ea8e6a9dc4e4c874fec7cfade2fe
“coffeeisamazing” <caleb.searles@libertymutual.com>
2016-11-18 09:27:17 -0500

    Add response.
    Add response.

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,6 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, comment, patient_id, doctor_id) VALUES
+('August 12, 2016', '10am', 'no comment', 8, 4),
+('August 17, 2016', '1pm', 'no comment', 5, 6),
+('August 12, 2016', '10am', 'no comment', 8, 6),
+('August 17, 2016', '1pm', 'no comment', 7, 4);
