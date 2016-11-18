02ad278f8048f5afa89f3d770e7c78763d7bea2a
rickdean <rickdean@outlook.com>
2016-11-18 09:30:04 -0500

    Commit Diagnostics Diagnostic commits

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE patients(
+id SERIAL PRIMARY KEY,
+surname TEXT,
+given_name TEXT,
+gender TEXT,
+born_on TEXT,
+insurance TEXT,
+);
+
+CREATE TABLE doctors(
+id SERIAL PRIMARY KEY,
+surname TEXT,
+given_name TEXT,
+gender TEXT,
+born_on TEXT,
+specialty TEXT,
+);
+
+CREATE TABLE appointments(
+id SERIAL PRIMARY KEY,
+date TEXT,
+time TEXT,
+comment TEXT,
+doctor_id
+patient_id
+);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,12 @@
 -- retrieve appointment information
+SELECT p.surname patient, a.time  appt_time
+FROM patients p
+INNER JOIN patients p
+  ON p.id = a.patient_id
+WHERE a.doctor_id IN ('7');
+
+SELECT d.surname doctor, a.time appt_time
+FROM doctors d
+INNER JOIN patients p
+  ON d.id = p.doctor_id
+WHERE d.surname IN ('Hubbard');
