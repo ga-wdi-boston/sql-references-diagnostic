07014a34f85972c018625b0f5155553fb4902055
sujkid <suja.sreekumar@yahoo.com>
2016-11-18 09:31:24 -0500

    Diagnostic done
    ok

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,30 @@
--- create doctors, patients, and appointments tables
+-- -- create doctors, patients, and appointments tables
+-- CREATE TABLE doctors(
+--   id SERIAL PRIMARY KEY,
+--   surname TEXT,
+--   given_name TEXT,
+--   gender TEXT,
+--   born_on DATE,
+--   specialty TEXT
+-- );
+--
+-- CREATE TABLE patients(
+--   id SERIAL PRIMARY KEY,
+--   surname TEXT,
+--   given_name TEXT,
+--   gender TEXT,
+--   born_on DATE,
+--   insurance TEXT
+-- );
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  date TEXT,
+  time TEXT,
+  comment TEXT,
+  doctor_id INTEGER REFERENCES dorctors(id),
+  patient_id INTEGER REFERENCES patients(id),
+);
+
+CREATE INDEX ON appointments(doctor_id);
+CREATE INDEX ON appointments(patient_id);
