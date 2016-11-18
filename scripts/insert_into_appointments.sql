c8902a1591d011a1b2a2a6a95de91bce95ed935a
kmg1434 <kmg1434@gmail.com>
2016-11-18 09:30:27 -0500

    SQL Diagnostic Response
    Comfort: 4
    Clarity: 3
    
    used yesterdays reference repo for reference, not sure about IDs yet.

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,12 @@
 -- insert appointments creating connections between doctors and patients
+
+
+-- I'm confused as to how I'm supposed to figure out the ID of the docotrs and
+-- patients without explicitly looking at the tables. If these tables were long
+-- in a real life situation, this would be extremely tedious.
+
+INSERT INTO appointments(app_date, app_time, doctor_id, patient_id, comment) VALUES
+  ('', ?, ?, 'comment'),
+  ('', ?, ?, 'comment'),
+  ('', ?, ?, 'comment'),
+  ('', ?, ?, 'comment');
