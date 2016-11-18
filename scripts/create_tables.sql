07cfafea0841b5a2a0021b7d3c73d08e9942eaf9
sphilippe7 <solomon924@gmail.com>
2016-11-18 09:27:16 -0500

    SQL References diagnostic on 11/18/16
    comfort: 1
    clarity: 3
    
    I need more practice with this. I could not get things to work at all

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,24 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(surname,given_name,gender,born_on,specialty)
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE
+  specialty TEXT,
+);
+
+CREATE TABLE patients(surname,given_name,gender,born_on,insurance)
+  id SERIAL PRIMARY KEY,
+  surname TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE
+  insurance TEXT,
+);
+
+CREATE TABLE appointments(date, time, comment)
+id SERIAL PRIMARY KEY,
+date DATE,
+time TEXT,
+comment TEXT,
