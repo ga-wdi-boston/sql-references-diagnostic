16d549159878e5a08b2ea40478577c5440bd3cb9
Antony Donovan <antony.donovan@generalassemb.ly>
2017-02-06 15:58:52 -0500

    Add solution
    One commit on 016/master

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+-- create doctors
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  dob DATE,
+  specialty TEXT
+);
+-- create patients
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  dob DATE,
+  insurance TEXT
+);
+-- create appointments
+CREATE TABLE appointments (
+  date DATE,
+  time TIME,
+  comment TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
