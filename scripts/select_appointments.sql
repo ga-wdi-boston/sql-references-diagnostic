2880a1abeb67b71b80af1d06035dbd18c13e10b2
jwaskow <thewasko@gmail.com>
2017-02-14 09:31:25 -0500

    Submit Response

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,16 @@
 -- retrieve appointment information
+SELECT a.time AS time, d.family_name
+  FROM appointments AS a
+    INNER JOIN doctors AS d
+      ON d.id = a.doctor_id
+    WHERE d.family_name = 'Hubbard'
+;
+
+SELECT d.family_name AS name AND a.time AS time, p.family_name, p.given_name
+  FROM appointments AS a
+    INNER JOIN doctors AS d
+      ON d.id = a.doctor_id
+    INNER JOIN patients AS p
+      ON p.id = a.patient_id
+    WHERE p.family_name = 'Wilcox' AND p.given_name = 'Marsha'
+;
