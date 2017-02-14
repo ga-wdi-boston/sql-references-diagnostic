92fbb3582f16e9ab4ef50ab39643ee606a290b62
ashlenprice <ashlen.price@gmail.com>
2017-02-14 09:31:10 -0500

    answer diagnostic
    comfort 2.5
    clarity 3

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,26 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE appointments(
+  date DATE,
+  time TIME,
+  comments TEXT
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctors(id)
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,27 @@
 -- insert appointments creating connections between doctors and patients
+
+
+INSERT INTO appointments(date, time, patient_id, doctor_id)
+SELECT DATE '2016-08-12', TIME '10:00', p.id, d.id
+  FROM patients AS p, doctors AS d
+  WHERE p.family_name = 'Wilcox'
+    AND p.given_name = 'Marsha'
+    AND d.family_name = 'Hilton'
+    AND d.given_name = 'Vicky'
+UNION
+SELECT '2016-08-17', '13:00', p.id, d.id
+FROM patients AS p
+  INNER JOIN doctors as d
+  ON p.family_name = 'Talley'
+    AND p.given_name = 'Ivory'
+    AND d.family_name = 'Hubbard'
+    AND d.given_name = 'Spencer'
+UNION
+
+  ;
+
+
+-- Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2016.
+-- Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+-- Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+-- Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
