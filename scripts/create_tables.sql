a71035b094689ec6f89c9bfd41dce2600902221c
cathyob <cathy.obrien09@gmail.com>
2017-02-14 09:29:51 -0500

    sql-references-diagnostic
    response to sql references diagnostic

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,31 @@
 -- create doctors, patients, and appointments tables
+
+-- family_name,given_name,gender,born_on,specialty
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT,
+);
+
+-- family_name,given_name,gender,born_on,insurance
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT,
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  date DATE,
+  time TIME,
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctors(id)
+);
+CREATE INDEX ON appointments(patient_id);
+CREATE INDEX ON appointments(doctor_id);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,36 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(start_at, length, doctor_id, patient_id)
+  SELECT TIMESTAMP '2016-08-12 10:00', p.id, d.id
+  FROM patients AS p, doctors AS d
+  WHERE p.family_name = 'Wilcox'
+    AND p.given_name = 'Marsha'
+    AND d.family_name = 'Hilton'
+    AND d.given_name = 'Vicky'
+    AND d.specialty = 'Internal Medicine'
+  UNION
+  SELECT TIMESTAMP '2016-08-17 13:00', p.id, d.id
+  FROM patients AS p
+    INNER JOIN doctors as d
+      ON p.family_name = 'Talley'
+        AND p.given_name = 'Ivory'
+        AND d.family_name = 'Hubbard'
+        AND d.given_name = 'Spencer'
+        AND d.specialty = 'Dermatology'
+  UNION
+  SELECT TIMESTAMP '2016-08-12 10:00', p.id, d.id
+  FROM patients AS p, doctors AS d
+  WHERE p.family_name = 'Wilcox'
+    AND p.given_name = 'Marsha'
+    AND d.family_name = 'Hubbard'
+    AND d.given_name = 'Spencer'
+    AND d.specialty = 'Dermatology'
+  UNION
+  SELECT TIMESTAMP '2016-08-17 13:00', p.id, d.id
+  FROM patients AS p
+    INNER JOIN doctors as d
+      ON p.family_name = 'Hodge'
+        AND p.given_name = 'Blake'
+        AND d.family_name = 'Hilton'
+        AND d.given_name = 'Vicky'
+        AND d.specialty = 'Internal Medicine'
+;
