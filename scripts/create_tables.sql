a0317fa7e020799f4097bcd95365f9ee500ddded
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-02-14 09:27:47 -0500

    Diagnostic - sql references
    comfort: 3
    clarity: 5
    
    Not enough time to do question 4, I think I know how to figure it out
    if more time given. I used previous repositories for this diagnostic.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,25 @@
 -- create doctors, patients, and appointments tables
+create table doctors (
+  id serial primary key,
+  family_name text,
+  given_name text,
+  gender text,
+  born_on date,
+  specialty text
+);
+
+create table patients (
+  id serial primary key,
+  family_name text,
+  given_name text,
+  gender text,
+  born_on date,
+  insurance text
+);
+
+create table appointments (
+  id serial primary key,
+  date_on date,
+  length integer,
+  comment text
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,30 @@
 -- insert appointments creating connections between doctors and patients
+insert into appointments(doctor_id, patient_id, date_on)
+ select doctors.id, patients.id, timestamp '2016-08-12 10:00'
+  from doctors, patients
+    where doctors.given_name = 'Vicky'
+    and doctors.family_name = 'Hilton'
+    and patients.given_name = 'Marsha'
+    and patients.given_name = 'Wilcox'
+  union
+  select doctors.id, patients.id, '2016-08-17 13:00'
+   from doctors, patients
+     where doctors.given_name = 'Spencer'
+     and doctors.family_name = 'Hubbard'
+     and patients.given_name = 'Ivory'
+     and patients.given_name = 'Talley'
+   union
+   select doctors.id, patients.id, '2016-08-12 10:00'
+    from doctors, patients
+      where doctors.given_name = 'Spencer'
+      and doctors.family_name = 'Hubbard'
+      and patients.given_name = 'Marsha'
+      and patients.given_name = 'Wilcox'
+   union
+    select doctors.id, patients.id, '2016-08-17 13:00'
+     from doctors, patients
+       where doctors.given_name = 'Vicky'
+       and doctors.family_name = 'Hilton'
+       and patients.given_name = 'Blake'
+       and patients.given_name = 'Hodge'
+  ;
