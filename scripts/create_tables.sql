d7284d659a0e581cd496cdd4fe2f74fc3915932b
novakw267 <novakw267@gmail.com>
2017-02-14 09:27:31 -0500

    Um, I tried.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,27 @@
 -- create doctors, patients, and appointments tables
+create table doctors(
+  id serial primary key
+  family_name text,
+   given_name text,
+    gender text,
+     born_on date,
+      specialty text
+    );
+
+create table patients (
+  id serial primary key
+    family_name text,
+     given_name text,
+      gender text,
+       born_on date,
+        insurance text
+);
+
+create table appointments(
+  id serial primary key,
+    date date,
+    time time,
+    comment  text,
+    doctor.id integer references doctors(id)
+    patient.id integer references patients(id)
+);
