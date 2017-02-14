aa4ab7f4c146ed2291d44eb3c271183d148ce672
Soboscott <scott.landry01772@yahoo.com>
2017-02-14 09:38:25 -0500

    response to diagnosis

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,24 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+
+);
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  insurance TEXT,
+  born_on DATE
+);
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  start_at DATE,
+  length TIMESTAMP,
+  comments TEXT
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,17 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(start_at, length, patient_id, doctor_id)
+  SELECT TIMESTAMP '2016-08-12 10:00', p.id, d.id
+    FROM patients AS p, doctors AS d
+    WHERE p.family_name = 'Wilcox '
+      AND p.given_name = 'Marsha'
+      AND d.given_name = 'Vicky'
+      AND d.family_name = 'Hilton'
+  UNION
+  SELECT '2016-08-17 13:00', p.id, d.id
+  FROM patients AS p
+    INNER JOIN doctors AS d
+      ON p.family_name = 'Talley '
+      AND p.given_name = 'Ivory'
+      AND d.given_name = 'Hubbard'
+      AND d.family_name = 'Spencer'
+;
