485093d760bbbf9a483c227233881e024480c335
ddobson <ddobson445@gmail.com>
2017-02-14 09:21:42 -0500

    Complete diagnostic Answers in scripts/

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,30 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY
+  start_on DATE,
+  start_at TIME,
+  comment TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
+
+CREATE INDEX ON appointments(doctor_id);
+CREATE INDEX ON appointments(patient_id);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,17 @@
 -- retrieve appointment information
+SELECT p.given_name, p.family, a.start_on, a.start_at
+FROM appointments a
+INNER JOIN patients p
+  ON p.id = a.patient_id
+INNER JOIN doctors d
+  ON d.id = a.doctor_id
+WHERE d.family_name = 'Hubbard';
+
+SELECT d.given_name, d.family, a.start_on, a.start_at
+FROM appointments a
+INNER JOIN patients p
+  ON p.id = a.patient_id
+INNER JOIN doctors d
+  ON d.id = a.doctor_id
+WHERE p.family_name = 'Wilcox'
+  AND p.given_name = 'Marsha';
