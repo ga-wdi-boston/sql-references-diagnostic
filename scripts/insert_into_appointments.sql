12d1331f554b75719a64cb1df0c7e11e29c197f7
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2017-04-24 10:55:58 -0400

    Update solution from diagnostic-template
    Refresh for cohort lm01

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,18 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, doctor_id, patient_id)
+  SELECT DATE '2017-08-12', TIME '1000',
+    (SELECT id FROM doctors WHERE family_name = 'Hilton'),
+    (SELECT id FROM patients WHERE family_name = 'Wilcox')
+  UNION
+  SELECT '2017-08-17', '1300',
+    (SELECT id FROM doctors WHERE family_name = 'Hubbard'),
+    (SELECT id FROM patients WHERE family_name = 'Talley')
+  UNION
+  SELECT '2017-08-17', '1000',
+    (SELECT id FROM doctors WHERE family_name = 'Hubbard'),
+    (SELECT id FROM patients WHERE family_name = 'Wilcox')
+  UNION
+  SELECT '2017-08-17', '1300',
+   (SELECT id FROM doctors WHERE family_name = 'Hilton'),
+   (SELECT id FROM patients WHERE family_name = 'Hodge')
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,17 @@
 -- retrieve appointment information
+-- for Dr. Hubbard
+SELECT p.family_name, a.date, a.time
+  FROM patients p
+  INNER JOIN appointments a
+    ON a.patient_id = p.id
+  INNER JOIN doctors d
+    ON a.doctor_id = d.id
+WHERE d.family_name = 'Hubbard';
+-- for patient Wilcox
+SELECT d.family_name, a.date, a.time
+  FROM doctors d
+  INNER JOIN appointments a
+    ON a.doctor_id = d.id
+  INNER JOIN patients p
+    ON a.patient_id = p.id
+WHERE p.family_name = 'Wilcox';
