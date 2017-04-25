4c9d127cb635069efe42c9bdc6356e21041f8d73
“conorjennings” <conorjennings@gmail.com>
2017-04-25 09:28:31 -0400

    SQL diagnostic morning quiz.

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,23 @@
 -- insert appointments creating connections between doctors and patients
+
+-- Create the following appointments by inserting the appropriate rows into the
+-- appointments table. Save your code in `insert_into_appointments.sql`:
+--
+-- -   Marsha Wilcox seeing Dr. Vicky Hilton at 10am on August 12, 2016.
+-- -   Ivory Talley seeing Dr. Spencer Hubbard at 1pm on August 17, 2016.
+-- -   Marsha Wilcox seeing Dr. Spencer Hubbard at 10am on August 12, 2016.
+-- -   Blake Hodge seeing Dr. Vicky Hilton at 1pm on August 17, 2016.
+
+-- Since Ive no tables created in pSQL, Here i assume that the doctor_id for Dr. Hilton is 1 and the patient Id for Wilcox is 2:
+INSERT INTO appointments (appt_date, appt_time, comments, doctor_id, patient_id ) VALUES ('2016-08-12', '10AM', 'Marsha Wilcox seeing Dr. Vicky Hilton', 1, 2);
+
+-- Since Ive no tables created in pSQL, Here i assume that the doctor_id for Dr. Hubbard is 2 and the patient Id for Talley is 3:
+INSERT INTO appointments (appt_date, appt_time, comments, doctor_id, patient_id ) VALUES ('2016-08-17', '1PM', 'Ivory Talley seeing Dr. Spencer Hubbard', 2, 3);
+
+
+-- Since Ive no tables created in pSQL, Here i assume that the doctor_id for Dr. Hubbard is 2 and the patient Id for Wilcox is 2:
+INSERT INTO appointments (appt_date, appt_time, comments, doctor_id, patient_id ) VALUES ('2016-08-12', '10AM', 'Marsha Wilcox seeing Dr. Spencer Hubbard', 2, 2);
+
+
+-- Since Ive no tables created in pSQL, Here i assume that the doctor_id for Dr. Hilton is 1 and the patient Id for Hodge is 4:
+INSERT INTO appointments (appt_date, appt_time, comments, doctor_id, patient_id ) VALUES ('2016-08-17', '1PM', 'Blake Hodge seeing Dr. Vicky Hilton', 1, 4);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,68 @@
 -- retrieve appointment information
+-- Write a query to retrieve the names and appointment times for everyone seeing
+-- Dr. Hubbard.
+
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  insurance TEXT
+);
+
+Here the doctor_id and patient_id is a foreign key from the two respective tables where it is the PK on those tables.
+CREATE TABLE appointments (
+id SERIAL PRIMARY KEY,
+appt_date DATE,
+appt_time TIME,
+comments TEXT,
+doctor_id,
+patient_id
+);
+
+SELECT
+  p.family_name,
+  p.given_name,
+  a.appt_time
+from
+  patients p
+inner join
+  appointments a
+on
+  p.id = a.patient_id
+inner join
+  doctors d
+on
+  d.id = a.doctor_id
+
+where
+  d.family_name = 'Hubbard';
+
+-- Then, write another to retrieve the Doctors' names and appointment
+SELECT
+  d.family_name,
+  d.given_name,
+  a.appt_time
+from
+  patients p
+inner join
+  appointments a
+on
+  p.id = a.patient_id
+inner join
+  doctors d
+on
+  d.id = a.doctor_id
+
+where
+  p.family_name = 'Wilcox'
+  and   p.given_name = 'Martha';
