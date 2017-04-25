6f354878b1b1e5ea73bcf2be12e08419eb6a548c
aeabbott <amandaeabbott@gmail.com>
2017-04-25 09:24:21 -0400

    Amanda's answers to sql references diagnostic

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,16 @@
 -- retrieve appointment information
+SELECT d.given_name, a.appt_time
+FROM appointments a
+  INNER JOIN doctors d
+  ON d.id = a.doctor_id
+  INNER JOIN patients p
+  ON p.id = a.patient_id
+WHERE d.family_name = 'Hubbard';
+
+SELECT p.given_name, a.appt_time
+  FROM patients p
+  INNER JOIN appointments a
+  ON p.id = a.patient_id
+  INNER JOIN doctors d
+  ON d.id = a.doctor_id
+  WHERE p.family_name = 'Wilcox';
