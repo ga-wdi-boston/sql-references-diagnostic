d6610258da0bfe59fb388600619d213c8807ee03
jkjellan <jkjellan@gmail.com>
2017-04-25 09:27:50 -0400

    Adds answers to sql-references diagnostic

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments(date,time,comment,patient_id,doctor_id) VALUES
+  ('2016-08-12', '10:00 AM','Marsha Wilcox seeing Dr. Vicky Hilton',8, 4),
+  ('2016-08-17', '1:00 PM','Ivory Talley seeing Dr. Spencer Hubbard',5, 6),
+  ('2016-08-12', '10:00 AM','Marsha Wilcox seeing Dr. Spencer Hubbard',8, 6),
+  ('2016-08-17', '1:00 PM','Marsha Wilcox seeing Dr. Vicky Hilton',7, 4);
