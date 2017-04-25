7202b14bbb3655edd8b3927e311277cba333ced5
alzateja <alzateja@gmail.com>
2017-04-25 09:27:26 -0400

    Response to SQL Reference Diagnostic
    Comfort:3
    Clarity:4
    
    I got confused at the outset and ran out of time.

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,31 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(appt_date, appt_time, doctor_id, patient_id)
+  SELECT DATE '2016-08-12', TIME '10:00', dr.id, pt.id
+  FROM patients pt, doctors dr ,appointments appt
+  WHERE pt.given_name = 'Marsha'
+    AND pt.family_name = 'Wilcox'
+    AND dr.given_name = 'Vicky'
+    AND dr.family_name = 'Hilton'
+  UNION
+  SELECT DATE '2016-08-17', TIME '13:00', dr.id, pt.id
+  FROM patients pt, doctors dr ,appointments appt
+  WHERE pt.given_name = 'Ivory'
+    AND pt.family_name = 'Talley'
+    AND dr.given_name = 'Spencer'
+    AND dr.family_name = 'Hubbard'
+    UNION
+    SELECT DATE '2016-08-12', TIME '10:00', dr.id, pt.id
+    FROM patients pt, doctors dr ,appointments appt
+    WHERE pt.given_name = 'Marsha'
+      AND pt.family_name = 'Wilcox'
+      AND dr.given_name = 'Spencer'
+      AND dr.family_name = 'Hubbard'
+      UNION
+      SELECT DATE '2016-08-17', TIME '13:00', dr.id, pt.id
+      FROM patients pt, doctors dr ,appointments appt
+      WHERE pt.given_name = 'Blake'
+        AND pt.family_name = 'Hodge'
+        AND dr.given_name = 'Vicky'
+        AND dr.family_name = 'Hilton'
+  ;
+;
