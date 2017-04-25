9299cb2951d15c9defb6105d912750933af85492
angmas <angela.mashlan@safeco.com>
2017-04-25 09:30:02 -0400

    added responses

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,23 @@
--- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+CREATE TABLE appointments(
+  appt_date DATE,
+  appt_time TIME,
+  comment TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
