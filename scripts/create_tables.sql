7048c225188e9b2bc14ffeebc0f45b2dc39f4811
sjruby <samuel.ruby@libertymutual.com>
2017-04-25 09:29:21 -0400

    Submitting Response

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,29 @@
 -- create doctors, patients, and appointments tables
+
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  specialty TEXT,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE
+);
+
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  insurance TEXT,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE
+);
+
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctors(id),
+  apointment_date DATE
+);
+
+CREATE INDEX ON appointments(patient_id);
+CREATE INDEX ON appointments(doctor_id);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
--- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments(patient_id,doctor_id,apointment_date, appointment_time) VALUES
+  (8, 4,'2016-08-12', '10:00 AM'),
+  (8, 6,'2016-08-12', '10:00 AM'),
+  (5, 6,'2016-08-17', '1:00 PM'),
+  (7, 4,'2016-08-12', '1:00 PM')
+;
