458a5f5b889a7f846f9827f6e02020df4be4cd99
cpm5041 <christopher.marcenelle@libertymutual.com>
2017-04-25 09:25:10 -0400

    SQL References
    Comfort 3
    Clarity 3
    
    I felt ok reading SQL and adapting it but would need more practice
    writing it on my own from scratch.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,28 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  appt_date DATE,
+  appt_time TIME,
+  comment TEXT,
+  patient_id INTEGER REFERENCES patients(id),
+  doctor_id INTEGER REFERENCES doctors(id)
+);
+CREATE INDEX ON appointments(patient_id);
+CREATE INDEX ON appointments(doctor_id);
+);
--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,30 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments(appt_date, appt_time, doctor_id, patient_id)
+  SELECT TIMESTAMP '2016-08-12', '10:00', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.given_name = 'Vicky'
+      AND d.family_name = 'Hilton'
+      AND p.given_name = 'Marsha'
+      AND p.family_name = 'Wilcox'
+INSERT INTO appointments(appt_date, appt_time, doctor_id, patient_id)
+  SELECT TIMESTAMP '2016-08-17', '13:00', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.given_name = 'Spencer'
+      AND d.family_name = 'Hubbard'
+      AND p.given_name = 'Ivory'
+      AND p.family_name = 'Talley'
+INSERT INTO appointments(appt_date, appt_time, doctor_id, patient_id)
+  SELECT TIMESTAMP '2016-08-12', '10:00', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.given_name = 'Spencer'
+      AND d.family_name = 'Hubbard'
+      AND p.given_name = 'Marsha'
+      AND p.family_name = 'Wilcox'
+INSERT INTO appointments(appt_date, appt_time, doctor_id, patient_id)
+  SELECT TIMESTAMP '2016-08-17', '13:00', d.id, p.id
+  FROM doctors d, patients p
+    WHERE d.given_name = 'Spencer'
+      AND d.family_name = 'Hubbard'
+      AND p.given_name = 'Blake'
+      AND p.family_name = 'Hodge'
