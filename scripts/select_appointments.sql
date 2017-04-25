699f80427e6149d462e5ca7bd4fe0b649c13917c
joehsuLM <joseph.hsu@libertymutual.com>
2017-04-25 09:28:41 -0400

    Added scripts to diagnostic

--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,19 @@
 -- retrieve appointment information
+SELECT patients.family_name,
+patients.given_name,
+appointments.appointment_date,
+appointments.appointment_time
+FROM patients
+  INNER JOIN appointments
+    ON patients.id = appointments.patient_id
+  INNER JOIN doctors
+    ON doctors.id = appointments.doctor_id
+    WHERE doctor_id = 6;
+
+SELECT doctors.family_name, doctors.given_name
+  FROM doctors
+    INNER JOIN appointments
+      ON doctors.id = appointments.doctor_id
+    INNER JOIN patients
+      ON patients.id = appointments.patient_id
+      WHERE patient_id = 8;
