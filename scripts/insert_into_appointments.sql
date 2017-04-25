acbd66bac15c93ad6e41135da6ffaf344aa5cf08
MattKwonDo <mgduvall@gmail.com>
2017-04-25 09:29:32 -0400

    response
    5 5

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,7 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments Values
+('12-08-2016','10:00','Marsha Wilcox seeing Dr. Vicky Hilton'),
+('08-17-2016','13:00','Ivory Talley seeing Dr. Spencer Hubbard'),
+('08-12-2016','10:00','Marsha Wilcox seeing Dr. Spencer Hubbard'),
+('08-17-2016','13:00','Blake Hodge seeing Dr. Vicky Hilton')
+;
