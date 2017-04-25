9b1a24efa87ce8da10bb9a179a849f259f246770
jim-moody <jim.moody@libertymutual.com>
2017-04-25 09:23:01 -0400

    Add responses to diagnostic
    Add script to create doctors, patients, appointments table
    Add script to insert appointments
    Add script to load doctors and patients from csv
    Add script to select appointments

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,9 @@
 -- insert appointments creating connections between doctors and patients
+
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id) VALUES
+  ('2016-08-12', '10:00:00', null, 4, 8),
+  ('2016-08-17', '13:00:00', null, 6, 5),
+  ('2016-08-12', '10:00:00', null, 6, 8),
+  ('2016-08-17', '13:00:00', null, 4, 7)
+
+;
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,25 @@
 -- retrieve appointment information
+SELECT
+p.given_name,
+p.family_name,
+a.time
+
+FROM patients p
+  JOIN appointments a ON a.patient_id = p.id
+  JOIN doctors d ON d.id = a.doctor_id
+
+WHERE d.family_name = 'Hubbard'
+;
+
+SELECT
+d.given_name,
+d.family_name,
+a.time
+
+FROM patients p
+  JOIN appointments a ON a.patient_id = p.id
+  JOIN doctors d ON d.id = a.doctor_id
+
+WHERE p.given_name = 'Marsha'
+AND p.family_name = 'Wilcox'
+;
