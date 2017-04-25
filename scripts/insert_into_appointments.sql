f9c70f7df621a31ebc9b1a9b587df7df63917fdd
jscohen <jon.cohen08@gmail.com>
2017-04-25 09:27:01 -0400

    Finished SQL Reference Lab

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,12 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(apt_date, apt_time, comment, patient_id, doctor_id) VALUES
+('2016-04-25','11:00','N/A',8,4);
+
+INSERT INTO appointments(apt_date, apt_time, comment, patient_id, doctor_id) VALUES
+('2016-04-25','11:00','N/A',5,6);
+
+INSERT INTO appointments(apt_date, apt_time, comment, patient_id, doctor_id) VALUES
+('2016-04-25','11:00','N/A',8,6);
+
+INSERT INTO appointments(apt_date, apt_time, comment, patient_id, doctor_id) VALUES
+('2016-04-25','11:00','N/A',7,4);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,5 @@
 -- retrieve appointment information
+SELECT * FROM appointments a
+INNER JOIN patients b ON a.patient_id = b.id
+INNER JOIN doctors c ON a.doctor_id = c.id
+WHERE c.family_name = 'Hubbard';
