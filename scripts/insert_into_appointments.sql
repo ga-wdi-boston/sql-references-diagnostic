5bada60bf5b84c25dff5559d168d16b58acbc372
buffalo0707 <alan.henderson@libertymutual.com>
2017-04-25 09:29:23 -0400

    did it

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,8 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(doctor_id, patient_id, appt_date, appt_time, comment)
+VALUES
+
+  (6, 5, '2016-08-17', '13:00:00', 'gross'),
+  (6, 8, '2016-08-12', '10:00:00', 'severe IBS'),
+  (4, 8, '2016-08-12', '10:0:00', 'serious case of the munchies'),
+  (4, 7, '2016-08-17','13:00:00', 'insurance fraud');
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,9 @@
 -- retrieve appointment information
+SELECT doctors.family_name, patients.given_name, patients.family_name, appointments.appt_time
+FROM doctors
+WHERE family_name = "Hubbard"
+INNER JOIN appointments
+  ON doctors.id = appointments.doctor_id
+INNER JOIN patients
+  ON appointments.patient_id = patients.id
+  ORDER BY doctors.family_name;
