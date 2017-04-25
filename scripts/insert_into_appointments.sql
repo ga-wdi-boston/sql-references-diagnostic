d561caa50e3f4c64f6928799b154356bf59c5ad5
dpowers59 <don.powers@yahoo.com>
2017-04-25 09:32:59 -0400

    Response

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,6 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date_on, time_on, doctor_id, patient_id)VALUES
+ (DATE '2016-Aug-12',TIME '10:00', 2, 3),
+ (DATE '2016-Aug-17',TIME '13:00'),  3, 4,
+ (DATE '2016-Aug-12',TIME '10:00', ),
+ (DATE '2016-Aug-17',TIME '13:00');
