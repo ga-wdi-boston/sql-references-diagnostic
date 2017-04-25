c97ce5a82be0814f9eec9806de1a0f029eae64ca
awoodrum87 <awoodrum87@gmail.com>
2017-04-25 09:34:29 -0400

    study for 4-25

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,28 @@
 -- create doctors, patients, and appointments tables
+-- create doctors, patients, and appointments tables
+
+CREATE TABLE doctors (
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  specialty TEXT
+);
+
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT,
+  gender TEXT,
+  born_on DATE,
+  insurance TEXT
+);
+
+CREATE TABLE appointments (
+  attending_physician TEXT references doctors(id),
+  patient_id TEXT references patients(id),
+  appt_date DATE,
+  appt_time TIME,
+  comments TEXT
+);
