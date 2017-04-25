9222f53472ed1c4e8b025d60e09272f5cb1001bf
antleo1995 <antleo1995@gmail.com>
2017-04-25 09:25:48 -0400

    SQL References
    Still at a bout a three with comfort, clarity is probably a 4. I get it
    but I still have a hard time writing these statements, and really feel
    like more guidance should have been given during these lectures.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,24 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors (
+ id SERIAL PRIMARY KEY,
+ family_name TEXT,
+ given_name TEXT,
+ gender TEXT,
+ born_on DATE,
+ specialty TEXT
+);
+CREATE TABLE patients (
+ id SERIAL PRIMARY KEY,
+ family_name TEXT,
+ given_name TEXT,
+ gender TEXT,
+ born_on DATE,
+ insurance TEXT
+);
+CREATE TABLE appointments (
+date_of DATE,
+time_of TIME,
+comments TEXT,
+doctor_id INTEGER REFERENCES doctors(id),
+patient_id INTEGER REFERENCES patients(id)
+);
