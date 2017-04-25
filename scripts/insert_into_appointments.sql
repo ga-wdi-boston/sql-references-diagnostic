02e04ad51f3344c7f9be197772473653941aaea1
vanduong28 <vanduong28@gmail.com>
2017-04-25 09:27:46 -0400

    Response to SQL references diagnostic
    Response to questions related to SQL references.

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,6 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments (patient_id, doctor_id, appointment_time, appointment_date) VALUES
+(8, 4, '10 AM', '2016-08-12'),
+(5, 6, '1 PM', '2016-08-17'),
+(8, 6, '10 AM', '2016-08-12'),
+(7, 4, '1 PM', '2016-08-17');
