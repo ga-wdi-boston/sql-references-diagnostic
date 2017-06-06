430f311ed6084f71ad413b5c11f1ba626effeed7
MarcyNash <marcywsa@gmail.com>
2017-06-06 09:29:14 -0400

    SQL references diagnostic

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,16 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+  SELECT DATE 'August 12, 2016', TIME '10am', '', d.id, p.id
+  FROM patients p doctors d
+  WHERE p.given_name = 'Marsha'
+    AND p.last_name = 'Wilcox'
+    AND d.given_name = 'Vicky'
+    AND d.last_name = 'Hilton';
+
+    INSERT INTO appointments(date, time, comment, doctor_id, patient_id)
+      SELECT DATE 'August 12, 2016', TIME '10am', '', d.id, p.id
+      FROM patients p doctors d
+      WHERE p.given_name = 'Marsha'
+        AND p.last_name = 'Wilcox'
+        AND d.given_name = 'Spencer'
+        AND d.last_name = 'Hubbard'
