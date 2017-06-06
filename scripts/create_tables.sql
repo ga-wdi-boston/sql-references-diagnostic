a9e911f33afc8fef257c11804606b8e23abe60b4
artpylon <matthewgray88@gmail.com>
2017-06-06 09:27:36 -0400

    Responses for sql references diag Matt G

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,1 +1,28 @@
 -- create doctors, patients, and appointments tables
+CREATE TABLE doctors(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT
+  born_on DATE,
+  specialty TEXT
+);
+CREATE TABLE patients(
+  id SERIAL PRIMARY KEY,
+  family_name TEXT,
+  given_name TEXT
+  born_on DATE,
+  insurance TEXT
+);
+-- Ambiguous if doctor and patient columns should be name or id, including both.
+CREATE TABLE appointments(
+  id SERIAL PRIMARY KEY,
+  date DATE,
+  time TIME
+  comment TEXT,
+  doctor TEXT,
+  patient TEXT,
+  doctor_id INTEGER REFERENCES doctors(id),
+  patient_id INTEGER REFERENCES patients(id)
+);
+CREATE INDEX ON appointments(patient_id);
+CREATE INDEX ON appointments(doctor_id);
--- scripts/select_appointments.sql
+++ scripts/select_appointments.sql
@@ -1,1 +1,9 @@
 -- retrieve appointment information
+
+SELECT patient, time, doctor
+FROM appointments
+WHERE doctor = 'Spencer Hubbard';
+
+SELECT doctor, time, patient
+FROM appointments
+WHERE patient = 'Marsha Wilcox';
