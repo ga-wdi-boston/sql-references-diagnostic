14bb0d32f311743249c7cd0c53d90e3528efc2a8
constaac <aconstantine719@gmail.com>
2017-06-06 09:26:03 -0400

    SQL Reference Diagnostic
    I felt extremely rushed. I kept running into small things that tripped
    me up. It asks for a data and time column for appointments, but SQL
    doesn't like that so I had do make something else up.
    
    I found the copy commands from a previous study, but if I had tried to
    spend time looking through the docs to get that one I would have been
    completely screwed. I definitely could have populated the appointments
    table with a select command if I had more time, but I usually reference
    previous studies for stuff like that. Our studies over the past couple
    weeks have been such a jumbled mess it was hard to find anything I was
    looking for even though I know it existed. Everything is named library,
    cookbook, and clinic, and half the code seems like it has holes in it.

--- scripts/insert_into_appointments.sql
+++ scripts/insert_into_appointments.sql
@@ -1,1 +1,6 @@
 -- insert appointments creating connections between doctors and patients
+INSERT INTO appointments(appt_date,appt_time,doctor_id,patient_id) VALUES
+ ('2016-08-12', '10:00:00', 4, 8),
+ ('2016-08-17', '13:00:00', 6, 5),
+ ('2016-08-12', '10:00:00', 6, 8),
+ ('2016-08-17', '13:00:00', 4, 7);
